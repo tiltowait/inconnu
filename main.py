b126f3a1a39066163e8f3ae2b5cfc9cc131c302d
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-04 11:02:40 -0700

    Fix and document reply bug

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 """
 main.py - Start up the bot and perform any last-minute configuration.
 Invite: https://discord.com/api/oauth2/authorize?client_id=882409882119196704&permissions=346176&scope=bot
+
+The discord-ui plugin API is fairly unstable and undergoing rapid change.
+The current best-working version is v4.2.7 (7d7e120608f6bd92a021a1c53f2039f6a0421a77)
+
+Current issues with discord-ui:
+
+* ctx.author.send issues when using a cached context
 """
 
 import os

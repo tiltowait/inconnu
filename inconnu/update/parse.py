72684b7fda65b0c19ce476dba7758689b28c22d2
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-03 20:02:46 -0700

    Allow character omission if the user has only one

--- inconnu/update/parse.py
+++ inconnu/update/parse.py
@@ -1,8 +1,8 @@
 """update/parse.py - Defines an interface for updating character traits."""
 
 from . import paramupdate
+from ..common import get_character
 from ..constants import character_db
-from ..databases import CharacterNotFoundError
 from ..display import parse as display
 
 __INSTRUCTIONS = "USAGE: `//update CHAR_NAME PARAMETER=NEW_VALUE ...`"
@@ -26,17 +26,25 @@ __KEYS = {
 
 
 async def parse(ctx, *args):
-    """Process the user's arguments."""
-    if len(args) < 2:
-        await ctx.reply(__INSTRUCTIONS)
+    """
+    Process the user's arguments.
+    Allow the user to omit a character if they have only one.
+    """
+    args = list(args) # To allow element removal
+    char_name, char_id = get_character(ctx.guild.id, ctx.author.id, args[0])
+
+    if char_id is None:
+        err = __character_error_message(ctx.guild.id, ctx.author.id, char_name)
+        err += f"\n\n{__INSTRUCTIONS}"
+
+        await ctx.reply(err)
         return
 
-    args = list(args) # To allow element deletion
-    char_name = args[0]
-    del args[0]
+    # Delete args[0] if it was the character name
+    if char_name.lower() == args[0].lower():
+        del args[0]
 
     try:
-        char_id = character_db.character_id(ctx.guild.id, ctx.author.id, char_name)
         parameters = __parse_arguments(*args)
 
         for parameter, new_value in parameters.items():
@@ -44,8 +52,7 @@ async def parse(ctx, *args):
 
         await display(ctx, char_name)
 
-
-    except (ValueError, CharacterNotFoundError) as err:
+    except ValueError as err:
         await ctx.reply(str(err))
 
 
@@ -83,3 +90,18 @@ def __update_character(guildid: int, userid: int, charid: int, param: str, value
     Raises ValueError if the parameter's value is invalid.
     """
     getattr(paramupdate, f"update_{param}")(guildid, userid, charid, value)
+
+
+def __character_error_message(guildid: int, userid: int, input_name: str) -> str:
+    """Create a message informing the user they need to supply a correct character."""
+    user_chars = list(character_db.characters(guildid, userid).keys())
+    message = None
+
+    if len(user_chars) == 0:
+        message = "You have no characters to update."
+    else:
+        user_chars = list(map(lambda char: f"`{char}`", user_chars))
+        message = f"You have no character named `{input_name}`. Options:\n\n"
+        message += ", ".join(user_chars)
+
+    return message

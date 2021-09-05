48484263d0553a7062e653643713f468cd709576
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-05 10:59:29 -0700

    Add help menus

---
+++ c_help.py
@@ -0,0 +1,161 @@
+"""c_help.py - Defines constants for help messages."""
+
+# Some of the help comments can get rather long, so we define them here rather
+# than in commands.py in order not to clutter up that file.
+
+# Gameplay commands
+
+ROLL_BRIEF = "Roll the dice"
+ROLL_DESC = "Roll a dice pool from explicit values or using stored traits."
+ROLL_USAGE = "[character] <pool> [hunger] [difficulty]"
+ROLL_HELP = """
+character:  The character performing the roll
+               Optional if you only have one character
+               Optional if you aren't rolling traits
+pool:       May use character traits and simple math.
+               Ex: 7
+               Ex: strength + brawl - 2
+               CHARACTER is mandatory when rolling traits
+hunger:     The number of hunger dice to roll (default 0)
+difficulty: The difficulty of the roll (default 0)
+"""
+
+ROUSE_BRIEF = "Perform a rouse check"
+ROUSE_USAGE="[character] [rouses]"
+ROUSE_HELP = """
+character:  The character performing the rouse check.
+               If this is supplied, any hunger increases
+               will automatically be applied.
+rouses:     The number of rouses to perform
+"""
+
+
+REMORSE_BRIEF = "Perform a remorse check"
+REMORSE_USAGE = "[character]"
+REMORSE_HELP = """
+character:  The character undergoing remorse
+               REQUIRED if you have more than one character
+               OPTIONAL if you have only one character
+"""
+
+
+RESONANCE_BRIEF = "Generate a random resonance"
+RESONANCE_HELP = RESONANCE_BRIEF + "."
+
+
+# Character CRUD commands
+
+CHAR_NEW_BRIEF = "Create a new character"
+CHAR_NEW_DESCRIPTION = "Create a new vampire, mortal, or ghoul."
+CHAR_NEW_USAGE = "name=NAME splat=SPLAT humanity=X hp=Y wp=Z"
+CHAR_NEW_HELP = """
+name:      The character's name
+splat:     vampire, mortal, or ghoul
+humanity:  Their Humanity rating
+hp:        Their max Health
+wp:        Their max Willpower
+
+Aside from tracking stats, characters let you roll traits, such as strength or occult. For more info:
+
+     //help v
+"""
+
+
+CHAR_DISPLAY_BRIEF = "Display a character's basic statistics"
+CHAR_DISPLAY_USAGE = "[character]"
+CHAR_DISPLAY_HELP = """
+character:  The character to display
+               Optional if you only have one character
+"""
+
+
+CHAR_UPDATE_BRIEF = "Update a character's basic statistics"
+CHAR_UPDATE_USAGE = "[character] <stat>=<value> ..."
+CHAR_UPDATE_HELP = """
+character:  The character to update
+               Optional if you only have one character
+stat:       The stat to update
+               Options:
+                  name
+                  humanity
+                  hunger
+                  hp
+                  wp
+                  cur_xp (current XP)
+                  total_xp
+"""
+
+
+CHAR_DELETE_BRIEF = "Delete a character"
+CHAR_DELETE_USAGE = "<character>"
+CHAR_DELETE_HELP = """
+character:  The character to delete
+
+You will be given a confirmation box before deletion occurs.
+"""
+
+
+# Trait CRUD stuff
+
+TRAITS_COMMAND_BRIEF = "Character trait management commands"
+TRAITS_COMMAND_USAGE = "<subcommand>"
+TRAITS_COMMAND_HELP = """
+Available subcommands:
+
+    add
+    list
+    update
+    delete
+"""
+
+
+TRAITS_ADD_BRIEF = "Add trait(s) to a character"
+TRAITS_ADD_USAGE = "[character] <trait>[=rating] ..."
+TRAITS_ADD_HELP = """
+character:  The character to add the traits to
+               Optional if you have only one character
+trait:      The name of the trait to add
+rating:     The trait's rating
+
+Multiple traits may be added at a time. If you omit RATING, then you will be prompted to supply ratings in DMs.
+
+If a character already has a listed trait, the command will result in an error. To update existing traits, use:
+
+     //traits update
+"""
+
+
+TRAITS_LIST_BRIEF = "Display a character's traits"
+TRAITS_LIST_USAGE = "[character]"
+TRAITS_LIST_HELP = """
+character:  The character whose traits will be shown
+
+Traits will not display in the main chat. Instead, a list will be DMed to you.
+"""
+
+
+TRAITS_UPDATE_BRIEF = "Update a character's trait(s)"
+TRAITS_UPDATE_USAGE = "[character] <trait>[=rating] ..."
+TRAITS_UPDATE_HELP = """
+character:  The character whose traits will be updated
+               Optional if you have only one character
+trait:      The name of the trait to update
+rating:     The trait's new rating
+
+Multiple traits may be updated at a time. If you omit RATING, then you will be prompted to supply ratings in DMs.
+
+If the character does not already have the traits, then the command will error. To add traits, use:
+
+     //traits add
+"""
+
+
+TRAITS_DELETE_BRIEF = "Remove traits from a character"
+TRAITS_DELETE_USAGE = "[character] <trait> ..."
+TRAITS_DELETE_HELP = """
+character:  The character from which to remove the traits
+               Optional if you have only one character
+trait:      The list of traits to remove, separated by spaces
+
+If the character does not possess one or more of the provided traits, the command will fail, with none of the traits being removed.
+"""
--- commands.py
+++ commands.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord_ui import UI, SelectedMenu
 
 import inconnu
+import c_help
 
 bot = commands.Bot(command_prefix="//", case_insensitive=True)
 _ = UI(bot)
@@ -12,25 +13,42 @@ _ = UI(bot)
 
 # Gameplay Commands
 
-@bot.command(name="v", aliases=["roll", "r"])
+@bot.command(
+    name="v", aliases=["roll", "r"],
+    brief=c_help.ROLL_BRIEF,
+    description=c_help.ROLL_DESC,
+    usage=c_help.ROLL_USAGE,
+    help=c_help.ROLL_HELP
+)
 async def roll(ctx, *, args=None):
     """Roll a dice pool, either raw or calculated from traits."""
     if args is not None:
         await inconnu.roll.parse(ctx, args)
 
-@bot.command()
+@bot.command(
+    brief = c_help.ROUSE_BRIEF,
+    usage=c_help.ROUSE_USAGE,
+    help=c_help.ROUSE_HELP
+)
 async def rouse(ctx, *args):
     """Perform a rouse check."""
     await inconnu.rousemorse.parse(ctx, "rouse", *args)
 
 
-@bot.command()
+@bot.command(
+    brief = c_help.REMORSE_BRIEF,
+    usage=c_help.REMORSE_USAGE,
+    help=c_help.REMORSE_HELP
+)
 async def remorse(ctx, *args):
     """Perform a remorse check."""
     await inconnu.rousemorse.parse(ctx, "remorse", *args)
 
 
-@bot.command()
+@bot.command(
+    brief = c_help.RESONANCE_BRIEF,
+    help=c_help.RESONANCE_HELP
+)
 async def resonance(ctx):
     """Perform a resonance check."""
     await inconnu.resonance.generate(ctx)
@@ -38,28 +56,49 @@ async def resonance(ctx):
 
 # Character CRUD
 
-@bot.command(name="new", aliases=["n"])
+@bot.command(
+    name="new", aliases=["n"],
+    brief = c_help.CHAR_NEW_BRIEF,
+    description = c_help.CHAR_NEW_DESCRIPTION,
+    usage = c_help.CHAR_NEW_USAGE,
+    help = c_help.CHAR_NEW_HELP
+)
 @commands.guild_only()
 async def new_character(ctx, *args):
     """Create a new character."""
     await inconnu.newchar.parse(ctx, *args)
 
 
-@bot.command(name="display", aliases=["d", "find", "f", "list", "l"])
+@bot.command(
+    name="display", aliases=["d", "find", "f", "list", "l"],
+    brief = c_help.CHAR_DISPLAY_BRIEF,
+    usage = c_help.CHAR_DISPLAY_USAGE,
+    help = c_help.CHAR_DISPLAY_HELP
+)
 @commands.guild_only()
 async def display_character(ctx, *args):
-    """Display a character's basic traits."""
+    """Display a character's basic traits"""
     await inconnu.display.parse(ctx, *args)
 
 
-@bot.command(name="update", aliases=["u", "up"])
+@bot.command(
+    name="update", aliases=["u", "up"],
+    brief = c_help.CHAR_UPDATE_BRIEF,
+    usage = c_help.CHAR_UPDATE_USAGE,
+    help = c_help.CHAR_UPDATE_HELP
+)
 @commands.guild_only()
 async def update_character(ctx, *args):
     """Update a character's parameters but not the traits."""
     await inconnu.update.parse(ctx, *args)
 
 
-@bot.command(name="delete", aliases=["del"])
+@bot.command(
+    name="delete", aliases=["del"],
+    brief = c_help.CHAR_DELETE_BRIEF,
+    usage = c_help.CHAR_DELETE_USAGE,
+    help = c_help.CHAR_DELETE_HELP
+)
 @commands.guild_only()
 async def delete_character(ctx, char_name):
     """Delete a character."""
@@ -68,31 +107,56 @@ async def delete_character(ctx, char_name):
 
 # Trait CRUD
 
-@bot.group(invoke_without_command=True, name="traits", aliases=["trait", "t"])
+@bot.group(
+    invoke_without_command=True, name="traits", aliases=["trait", "t"],
+    brief = c_help.TRAITS_COMMAND_BRIEF,
+    usage = c_help.TRAITS_COMMAND_USAGE,
+    help = c_help.TRAITS_COMMAND_HELP
+)
 async def modify_traits(ctx):
     """Traits subcommand start."""
     await ctx.reply("Uh, you need to do more, dude")
 
 
-@modify_traits.command(name="add")
+@modify_traits.command(
+    name="add",
+    brief = c_help.TRAITS_ADD_BRIEF,
+    usage = c_help.TRAITS_ADD_USAGE,
+    help = c_help.TRAITS_ADD_HELP
+)
 async def add_trait(ctx, *args):
     """Add trait(s) to a character."""
     await inconnu.traits.add_update.parse(ctx, False, *args)
 
 
-@modify_traits.command(name="list", aliases=["show", "s"])
+@modify_traits.command(
+    name="list", aliases=["show", "s"],
+    brief = c_help.TRAITS_LIST_BRIEF,
+    usage = c_help.TRAITS_LIST_USAGE,
+    help = c_help.TRAITS_LIST_HELP
+)
 async def list_traits(ctx, *args):
     """Display a character's traits."""
     await inconnu.traits.show.parse(ctx, *args)
 
 
-@modify_traits.command(name="update")
+@modify_traits.command(
+    name="update",
+    brief = c_help.TRAITS_UPDATE_BRIEF,
+    usage = c_help.TRAITS_UPDATE_USAGE,
+    help = c_help.TRAITS_UPDATE_HELP
+)
 async def update_traits(ctx, *args):
     """Update a character's trait(s)."""
     await inconnu.traits.add_update.parse(ctx, True, *args)
 
 
-@modify_traits.command(name="delete", aliases=["rm"])
+@modify_traits.command(
+    name="delete", aliases=["rm"],
+    brief = c_help.TRAITS_DELETE_BRIEF,
+    usage = c_help.TRAITS_DELETE_USAGE,
+    help = c_help.TRAITS_DELETE_HELP
+)
 async def delete_traits(ctx, *args):
     """Remove traits from a character."""
     await inconnu.traits.delete.parse(ctx, *args)

244e4779a68ac24b44ffed76a3ebfa4463aa3c77
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-05 12:45:37 -0700

    Update best discord-ui version reference

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ main.py - Start up the bot and perform any last-minute configuration.
 Invite: https://discord.com/api/oauth2/authorize?client_id=882409882119196704&permissions=346176&scope=bot
 
 The discord-ui plugin API is fairly unstable and undergoing rapid change.
-The current best-working version is v4.2.8 (5b948542f519b35ec7233749016044735b374c61)
+The current best-working version is v4.2.14
 """
 
 import os

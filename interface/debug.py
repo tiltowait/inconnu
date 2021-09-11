ffec207d35045d8c08d66c118a06f3e36ec6456e
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-11 10:43:09 -0700

    Tell us if we are in debug mode

--- interface/debug.py
+++ interface/debug.py
@@ -7,3 +7,6 @@ from discord_ui.tools import MISSING
 # When set, slash commands will only work on the Inconnu Support server. Therefore,
 # $INCONNU_DEV should not be set on the production server.
 WHITELIST = [882411164468932609, 826628660450689074] if "INCONNU_DEV" in os.environ else MISSING
+
+if isinstance(WHITELIST, list):
+    print("Limiting commands to the following guilds:", WHITELIST)

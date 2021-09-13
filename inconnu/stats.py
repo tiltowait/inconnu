04d78b534294b5221d7e786b3bbf93dea0047e9c
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-13 13:20:18 -0700

    Rename statistics collection

--- inconnu/stats.py
+++ inconnu/stats.py
@@ -141,5 +141,5 @@ class Stats:
             if Stats._CLIENT is None:
                 mongo = pymongo.MongoClient(os.environ["MONGO_URL"])
                 Stats._CLIENT = mongo
-                Stats._STATS = mongo.inconnu.statistics
+                Stats._STATS = mongo.inconnu.roll_stats
                 Stats._GUILDS = mongo.inconnu.guilds

19016243dc4d6e9862b6570b921eb1afbe2d3389
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-09-16 17:45:42 -0700

    Improve output and fix hunger 5 bug

--- inconnu/misc/mend.py
+++ inconnu/misc/mend.py
@@ -1,10 +1,13 @@
 """misc/mend.py - Mend Superficial damage."""
 
 import random
+from types import SimpleNamespace
+
+import discord
 
 from .. import common
-from .. import constants
-from ..character import update as char_update
+from ..constants import DAMAGE
+from ..character.display import trackmoji
 
 __HELP_URL = "https://www.inconnu-bot.com/#/additional-commands?id=mending-damage"
 
@@ -14,24 +17,51 @@ async def mend(ctx, character=None):
     try:
         tip = "`/mend` `character:CHARACTER`"
         character = await common.fetch_character(ctx, character, tip, __HELP_URL)
+        outcome = __heal(character)
 
-        if character.hunger == 5:
-            await ctx.respond(f"Can't mend. **{character.name}'s** Hunger is at 5!", hidden=True)
+        if isinstance(outcome, str):
+            await ctx.respond(outcome, hidden=True)
         else:
-            superficial = character.health.count(constants.DAMAGE.superficial)
-            if superficial == 0:
-                await ctx.respond("No damage to mend!", hidden=True)
-                return
-
-            mending = min(superficial, character.mend_amount)
-            update_string = f"sh=-{mending}"
-            update_message = f"Mending **{mending}** damage."
-
-            if random.randint(1, 10) < 6:
-                update_string += " hunger=+1"
-                update_message += " Hunger increased by **1**."
-
-            await char_update(ctx, update_string, character.name, update_message)
+            await __display_outcome(ctx, character, outcome)
 
     except common.FetchError:
         pass
+
+async def __display_outcome(ctx, character, outcome):
+    """Display the results of the mend."""
+    embed = discord.Embed(
+        title=f"Mended {outcome.mended} damage | Rouse {'Success' if outcome.rouse else 'Failure'}"
+    )
+    embed.set_author(name=character.name, icon_url=ctx.author.avatar_url)
+
+    embed.add_field(name="Health", value=trackmoji.emojify_track(character.health), inline=False)
+    embed.add_field(name="Hunger", value=trackmoji.emojify_hunger(character.hunger), inline=False)
+
+    if outcome.frenzy:
+        embed.set_footer(text="ROLL FOR HUNGER FRENZY!")
+
+    await ctx.respond(embed=embed)
+
+
+def __heal(character):
+    """Heal the character and perform the Rouse check."""
+    superficial = character.health.count(DAMAGE.superficial)
+    if superficial == 0:
+        return f"**{character.name}** has no Superficial damage to mend!"
+
+    mending = min(character.mend_amount, superficial)
+    superficial -= mending
+    aggravated = character.health.count(DAMAGE.aggravated)
+    unhurt = len(character.health) - superficial - aggravated
+
+    track = DAMAGE.none * unhurt + DAMAGE.superficial * superficial + DAMAGE.aggravated * aggravated
+    character.health = track
+
+    rouse = random.randint(1, 10) >= 6
+    if not rouse:
+        frenzy = character.hunger == 5
+        character.hunger += 1
+    else:
+        frenzy = False
+
+    return SimpleNamespace(mended=mending, rouse=rouse, frenzy=frenzy)

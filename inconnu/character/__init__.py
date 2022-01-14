2aeded891ced3126af8430660b56b8811a28961c
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-01-13 16:25:50 -0800

    Fix imports

--- inconnu/character/__init__.py
+++ inconnu/character/__init__.py
@@ -1,6 +1,6 @@
 """character - Facilities for character CRUD. This package does not contain VChar."""
 
 from .create import create
-from .display import display, display_requested
+from .display import display, display_requested, HEALTH, WILLPOWER, HUMANITY, POTENCY, HUNGER, EXPERIENCE, SEVERITY
 from .update import update, update_help
 from .delete import delete
--- inconnu/character/display/__init__.py
+++ inconnu/character/display/__init__.py
@@ -1,4 +1,4 @@
 """Set up the package interface."""
 
-from .display import display, display_requested
+from .display import display, display_requested, HEALTH, WILLPOWER, HUMANITY, POTENCY, HUNGER, EXPERIENCE, SEVERITY
 from . import trackmoji

90a1ca085c66efc639b5710fc6a8cc3ddcc1bc54
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-01-22 14:42:10 -0800

    Fix timeout

--- inconnu/character/create/wizard.py
+++ inconnu/character/create/wizard.py
@@ -5,12 +5,36 @@ import asyncio
 import os
 
 import discord
-from discord_ui import Button
+from discord_ui import Button, Listener
 from discord_ui.components import LinkButton
 
 from ...constants import INCONNU_ID
 from ...settings import Settings
 from ...vchar import VChar
+
+
+class _TraitListener(Listener):
+    """Listen to trait selection events."""
+
+    def __init__(self, callback, failback):
+        super().__init__(timeout=12)
+
+        self.callback = callback
+        self.failback = failback
+
+
+    @Listener.button()
+    async def respond_to_button(self, btn):
+        """Respond to the button event and send the button's rating to the callback."""
+        await btn.respond()
+        await self.callback(int(btn.component.label))
+
+
+    def _stop(self):
+        """Stop listening for events and send the failback."""
+        super()._stop()
+        asyncio.create_task(self.failback())
+
 
 class Wizard:
     """A helper class that guides a user through the chargen process."""
@@ -19,6 +43,7 @@ class Wizard:
         [Button(str(n)) for n in range(1,6)],
         [Button("0", color="gray")]
     ]
+
 
     def __init__(self, ctx, parameters):
         if "INCONNU_DEV" in os.environ:
@@ -87,12 +112,12 @@ class Wizard:
             character.add_trait(trait, rating)
 
         if self.use_accessibility:
-            await self.__finalixe_text(character)
+            await self.__finalize_text(character)
         else:
-            await self.__finalixe_embed(character)
+            await self.__finalize_embed(character)
 
 
-    async def __finalixe_text(self, character):
+    async def __finalize_text(self, character):
         """Display finalizing message in plain text."""
         contents = f"Success! {character.name} has been created in {self.ctx.guild.name}!"
         contents += f"\nMake a mistake? Use `/traits update` on {self.ctx.guild.name} to fix."
@@ -106,7 +131,7 @@ class Wizard:
         await self.msg.edit(content=contents, components=[button])
 
 
-    async def __finalixe_embed(self, character):
+    async def __finalize_embed(self, character):
         """Display finalizing message in an embed."""
         embed = discord.Embed(
             title="Success!",
@@ -142,20 +167,8 @@ class Wizard:
         else:
             await self.__query_embed(message)
 
-        # Await the user response
-        try:
-            btn = await self.msg.wait_for("button", self.ctx.bot, timeout=120)
-            await btn.respond()
-
-            rating = int(btn.component.label)
-            await self.__assign_next_trait(rating)
-
-        except asyncio.exceptions.TimeoutError:
-            await self.msg.edit(components=None)
-            err = f"Due to inactivity, your chargen on **{self.ctx.guild.name}** has been canceled."
-            await self.ctx.author.send(err)
-        #finally:
-            #await query_msg.disable_components()
+        if message is None:
+            _TraitListener(self.__assign_next_trait, self._timeout).attach_me_to(self.msg)
 
 
     async def __query_text(self, message=None):
@@ -195,3 +208,9 @@ class Wizard:
             self.msg = await self.ctx.author.send(embed=embed, components=Wizard._BUTTONS)
         else:
             await self.msg.edit(embed=embed)
+
+
+    async def _timeout(self):
+        """Inform the user they took too long."""
+        errmsg = f"Due to inactivity, your chargen on **{self.ctx.guild.name}** has been canceled."
+        await self.msg.edit(content=errmsg, embed=None, components=None)

41029e4892766c4c47b10e067019cb092c50da8a
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-01-27 11:36:03 -0800

    Pycord migration

--- interface/settings.py
+++ interface/settings.py
@@ -1,10 +1,8 @@
 """interface/settings.py - Settings-related commands."""
 
 import discord
-
+from discord.commands import Option, OptionChoice, slash_command
 from discord.ext import commands
-from discord_ui import SlashOption
-from discord_ui.cogs import slash_command
 
 import inconnu
 from . import debug
@@ -13,19 +11,18 @@ from . import debug
 class SettingsCommands(commands.Cog):
     """Settings-related commands."""
 
-    @slash_command(
-        options=[
-            SlashOption(int, "enable",
-                choices=[
-                    ("Yes", 1),
-                    ("No", 0)
-                ],
-                required=True
-            )
-        ],
-        guild_ids=debug.WHITELIST
-    )
-    async def accessibility(self, ctx, enable):
+    @slash_command()
+    @commands.guild_only()
+    async def accessibility(
+        self,
+        ctx: discord.ApplicationContext,
+        enable: Option(int, "Enable accessibility mode for yourself",
+            choices=[
+                OptionChoice("Yes", 1),
+                OptionChoice("No", 0)
+            ]
+        )
+    ):
         """Enable or disable accessibility mode for yourself."""
         enable = bool(enable)
         response = inconnu.settings.set_accessibility(ctx, enable, "user")
@@ -33,55 +30,46 @@ class SettingsCommands(commands.Cog):
         await ctx.respond(response)
 
 
-    @slash_command(
-        name="set",
-        options=[
-            SlashOption(int, "oblivion_stains",
-                description="Which Rouse results should give Oblivion stain warnings",
-                choices=[
-                    ("1s and 10s (RAW)", 100),
-                    ("1s only", 1),
-                    ("10s only", 10),
-                    ("Never", 0)
-                ]
-            ),
-            SlashOption(int, "accessibility",
-                description="Whether to enable or disable accessibility",
-                choices=[
-                    ("Yes", 1),
-                    ("No", 0)
-                ]
-            ),
-        ],
-        guild_ids=debug.WHITELIST
-    )
-    async def set(self, ctx, oblivion_stains=None, accessibility=None):
+    @slash_command()
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    async def set(
+        self,
+        ctx: discord.ApplicationContext,
+        oblivion_stains: Option(int, "Which Rouse results should give Oblivion stain warnings",
+            choices=[
+                OptionChoice("1s and 10s (RAW)", 100),
+                OptionChoice("1s only", 1),
+                OptionChoice("10s only", 10),
+                OptionChoice("Never", 0)
+            ]
+        ),
+        accessibility: Option(int, "Whether to enable or disable accessibility",
+            choices=[
+                OptionChoice("Yes", 1),
+                OptionChoice("No", 0)
+            ]
+        )
+    ):
         """(Admin-only) Assign server-wide settings."""
-        try:
-            responses = []
+        responses = []
 
-            if oblivion_stains is not None:
-                response = inconnu.settings.set_oblivion_stains(ctx, oblivion_stains)
-                responses.append(response)
+        if oblivion_stains is not None:
+            response = inconnu.settings.set_oblivion_stains(ctx, oblivion_stains)
+            responses.append(response)
 
-            if accessibility is not None:
-                accessibility = bool(accessibility)
-                response = inconnu.settings.set_accessibility(ctx, accessibility, "guild")
-                responses.append(response)
+        if accessibility is not None:
+            accessibility = bool(accessibility)
+            response = inconnu.settings.set_accessibility(ctx, accessibility, "guild")
+            responses.append(response)
 
-            if len(responses) > 0:
-                await ctx.respond("\n".join(responses))
-            else:
-                await self.settings(ctx)
-
-        except PermissionError as err:
-            await ctx.respond(err, hidden=True)
+        if len(responses) > 0:
+            await ctx.respond("\n".join(responses))
+        else:
+            await self.settings(ctx)
 
 
-    @slash_command(
-        name="settings",
-        guild_ids=debug.WHITELIST
-    )
+    @slash_command()
     async def settings(self, ctx):
         """Display the settings in effect."""
         accessibility = "ON" if inconnu.settings.accessible(ctx.author) else "OFF"

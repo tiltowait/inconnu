5a219aaed127c026792c8ea3a7efbe9d65462589
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-01-27 14:29:22 -0800

    Make it actually work

--- inconnu/views/__init__.py
+++ inconnu/views/__init__.py
@@ -4,4 +4,5 @@ from .basicselector import BasicSelector
 from .disablingview import DisablingView
 from .dropdown import Dropdown
 from .frenzyview import FrenzyView
+from .ratingview.py import RatingView
 from .traitsview import TraitsView
---
+++ inconnu/views/ratingview.py
@@ -0,0 +1,36 @@
+"""views/ratingselector.py - A view for selecting trait ratings."""
+
+import discord
+
+
+class RatingView(discord.ui.View):
+    """A View that lets the user select a rating."""
+
+    def __init__(self, callback):
+        super().__init__(timeout=120)
+        self.handler = callback
+
+        for rating in range(1, 6):
+            button = discord.ui.Button(
+                label=str(rating),
+                custom_id=str(rating),
+                row=0
+            )
+            button.callback = self.button_pressed
+            self.add_item(button)
+
+        button = discord.ui.Button(
+            label="0",
+            custom_id="0",
+            style=discord.ButtonStyle.secondary, row=1
+        )
+        button.callback = self.button_pressed
+        self.add_item(button)
+
+
+    async def button_pressed(self, interaction):
+        """Respond to the button."""
+        await interaction.response.pong()
+
+        rating = int(interaction.data["custom_id"])
+        await self.handler(rating)

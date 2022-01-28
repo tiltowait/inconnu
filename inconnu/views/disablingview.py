822d3a8d5a54a4054f04c49c4b93fcc09608facb
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-01-28 11:12:07 -0800

    Don't disable or remove link buttons

--- inconnu/views/disablingview.py
+++ inconnu/views/disablingview.py
@@ -4,16 +4,17 @@ from discord.ui import View
 
 
 class DisablingView(View):
-    """A view that can disable all its buttons."""
+    """A view that can disable all its buttons, save for its link buttons."""
 
     def __init__(self, timeout=60, remove_on_timeout=False):
         super().__init__(timeout=timeout)
         self.remove_on_timeout = remove_on_timeout
+        self.link_filter = filter(lambda btn: btn.url is None, self.children)
         self.message = None
 
     async def disable_items(self, interaction):
-        """Disable all items."""
-        for child in self.children:
+        """Disable all non-link items."""
+        for child in self.link_filter:
             child.disabled = True
 
         await interaction.response.edit_message(view=self)
@@ -24,9 +25,10 @@ class DisablingView(View):
         """Disable the components on timeout, if we have the view's message."""
         if self.message is not None:
             if self.remove_on_timeout:
-                self.clear_items()
+                for button in self.link_filter:
+                    self.remove_item(button)
             else:
-                for child in self.children:
+                for child in self.link_filter:
                     child.disabled = True
 
             if hasattr(self.message, "edit"):

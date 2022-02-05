15afc121c61d78410f622054a5313488aca72448
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-02-04 21:22:01 -0800

    Stringify mentions in comments

--- inconnu/vr/parse.py
+++ inconnu/vr/parse.py
@@ -33,7 +33,7 @@ async def parse(ctx, raw_syntax: str, comment: str, character: str, player: disc
     # Comments appear after the first # in a command
     if "#" in syntax:
         syntax, comment = syntax.split("#", 1)
-        comment = comment.strip()
+        comment = await stringify_mentions(ctx, comment)
 
     if comment is not None and (comment_len := len(comment)) > 300:
         await common.present_error(ctx, f"Comment is too long by {comment_len - 300} characters.")
@@ -257,3 +257,50 @@ def __combine_operators(stack):
 def needs_character(syntax: str):
     """Determines whether a roll needs a character."""
     return re.search(r"[A-z_]", syntax) is not None
+
+
+async def stringify_mentions(ctx, sentence):
+    """Replace all raw mentions and channels with their plaintext names."""
+    if not sentence:
+        return None
+
+    # Use a set to avoid redundant lookups
+    if (matches := set(re.findall(r"<[@#]!?\d+>", sentence))):
+        member_converter = discord.ext.commands.MemberConverter()
+        channel_converter = discord.ext.commands.GuildChannelConverter()
+
+        replacements = {}
+        failed_lookups = 0
+
+        for match in matches:
+            if match in replacements:
+                continue
+
+            if "@" in match:
+                # Member lookup
+                try:
+                    replacement = await member_converter.convert(ctx, match)
+                    replacements[match] = "@" + replacement.display_name
+                except discord.ext.commands.MemberNotFound:
+                    pass
+            else:
+                # Channel lookup
+                try:
+                    replacement = await channel_converter.convert(ctx, match)
+                    replacements[match] = "#" + replacement.name
+                except discord.ext.commands.BadArgument:
+                    pass
+
+            # Realistically, there should be no failed lookups. If there are,
+            # the user is probably trying to lock up the bot. Give them three
+            # tries in case there's something weird going on before bailing.
+            if not match in replacements:
+                failed_lookups += 1
+                if failed_lookups == 3:
+                    break
+
+        # Replace the items in the original string
+        for (match, replacement) in replacements.items():
+            sentence = sentence.replace(match, replacement)
+
+    return " ".join(sentence.split())

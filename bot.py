955e47e653ddd96cc8bbe9ad8b34f240a1345927
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-02-21 16:51:18 -0800

    Check error properties

--- bot.py
+++ bot.py
@@ -38,13 +38,15 @@ async def on_ready():
 @bot.event
 async def on_application_command_error(ctx, error):
     """Handle various errors we might encounter."""
-    if isinstance(error.original, commands.NoPrivateMessage):
+    error = getattr(error, "original", error) # Some pycord errors have `original`, but not all do
+
+    if isinstance(error, commands.NoPrivateMessage):
         await ctx.respond("Sorry, this command isn't available in DMs!", ephemeral=True)
         return
-    if isinstance(error.original, commands.MissingPermissions):
+    if isinstance(error, commands.MissingPermissions):
         await ctx.respond("Sorry, you don't have permission to do this!", ephemeral=True)
         return
-    if isinstance(error.original, discord.errors.NotFound):
+    if isinstance(error, discord.errors.NotFound):
         # This just means a button tried to disable when its message no longer exists.
         # We don't care, and there's nothing we can do about it anyway.
         pass

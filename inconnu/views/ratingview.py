d432dcb340f0cf8f9bf3f2722ccb7e9e56e028d1
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-02-23 13:45:10 -0800

    Simplify

--- inconnu/views/ratingview.py
+++ inconnu/views/ratingview.py
@@ -30,23 +30,17 @@ class RatingView(discord.ui.View):
             button.callback = self.button_pressed
             self.add_item(button)
 
-        # Button 0 is on a separate row
-        button_id = str(uuid.uuid4())
-        self.ratings[button_id] = rating
-
-        button = discord.ui.Button(
-            label="0",
-            custom_id=button_id,
-            row=1
-        )
-        button.callback = self.button_pressed
-        self.add_item(button)
+        # Button 0 is on a separate row. We don't give it a custom ID, because
+        # we just fall back to 0 if we can't find the rating anyway
+        zero_button = discord.ui.Button(label="0", row=1)
+        zero_button.callback = self.button_pressed
+        self.add_item(zero_button)
 
 
     async def button_pressed(self, interaction):
         """Respond to the button."""
         button_id = interaction.data["custom_id"]
-        rating = self.ratings[button_id]
+        rating = self.ratings.get(button_id, 0)
 
         await self.callback(rating)
 

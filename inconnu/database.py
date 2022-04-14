1196c8b1d92cc112a546c3b00c05ee4b7d37284f
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-04-13 17:06:44 -0700

    Make raw db private

--- inconnu/database.py
+++ inconnu/database.py
@@ -8,15 +8,15 @@ from umongo.frameworks import MotorAsyncIOInstance
 _mongoclient = motor.motor_asyncio.AsyncIOMotorClient(
     os.getenv("MONGO_URL"), serverSelectionTimeoutMS=1800
 )
-db = _mongoclient.inconnu
+_db = _mongoclient.inconnu
 
 # Raw collections
-characters = db.characters
-guilds = db.guilds
-log = db.log
-probabilities = db.probabilities
-users = db.users
-rolls = db.rolls
+characters = _db.characters
+guilds = _db.guilds
+log = _db.log
+probabilities = _db.probabilities
+users = _db.users
+rolls = _db.rolls
 
 # Instance used for umongo
-instance = MotorAsyncIOInstance(db)
+instance = MotorAsyncIOInstance(_db)

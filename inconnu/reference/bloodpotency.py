9788b1b08f8e14fb8540bf8c0b883938b9994a62
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-05-27 15:30:50 -0700

    Add generation requirements

--- inconnu/reference/bloodpotency.py
+++ inconnu/reference/bloodpotency.py
@@ -10,21 +10,28 @@ import inconnu.settings
 async def blood_potency(ctx, rating: int):
     """Display the Blood Potency rating."""
     potency = _RATINGS[rating]
+    generations = potency["generations"]
 
-    # Build the fields for text or embed display
-    fields = []
+    if generations == [4]:
+        description = "Available only to the 4th Generation."
+    else:
+        description = "Available to Generations " + " through ".join(map(str, generations)) + "."
 
+    embed = discord.Embed(title=f"Blood Potency {rating}", description=description)
+    embed.set_footer(text="V5 Core p.216")
+
+    # Add the fields
     surge = "Add " + inconnu.common.pluralize(potency["surge"], "die")
-    fields.append(("Blood Surge", surge))
+    embed.add_field(name="Blood Surge", value=surge)
 
     mend = inconnu.common.pluralize(potency["mend"], "pt")
-    fields.append(("Damage Mended", f"{mend}. Superficial Damage"))
+    embed.add_field(name="Damage Mended", value=f"{mend}. Superficial Damage")
 
     if (bonus := potency["bonus"]) == 0:
         bonus = "None"
     else:
         bonus = f"+{bonus}"
-    fields.append(("Power Bonus", bonus))
+    embed.add_field(name="Power Bonus", value=bonus)
 
     reroll = potency["reroll"]
     match reroll:
@@ -34,42 +41,33 @@ async def blood_potency(ctx, rating: int):
             reroll = "Level 1"
         case _:
             reroll = f"Level {reroll} and below"
-    fields.append(("Discipline Re-Roll", reroll))
+    embed.add_field(name="Discipline Re-Roll", value=reroll)
 
-    fields.append(("Bane Severity", potency["severity"]))
-    fields.append(("Feeding Penalty", potency["penalty"]))
-
-    # Display it!
-
-    if await inconnu.settings.accessible(ctx):
-        await __display_text(ctx, rating, fields)
-    else:
-        await __display_embed(ctx, rating, fields)
-
-
-async def __display_embed(ctx, rating, fields):
-    """Display the Blood Potency in an embed."""
-    embed = discord.Embed(title=f"Blood Potency {rating}")
-    embed.set_footer(text="V5 Core p.216")
-
-    for field, value in fields:
-        embed.add_field(name=field, value=value, inline=True)
+    embed.add_field(name="Bane Severity", value=potency["severity"])
+    embed.add_field(name="Feeding Penalty", value=potency["penalty"])
 
     await ctx.respond(embed=embed)
 
 
-async def __display_text(ctx, rating, fields):
-    """Display the Blood Potency in plain text."""
-    message = f"**Blood Potency {rating}**\n\n"
-    message += "\n".join(map(lambda f: f"**{f[0]}:** {f[1]}", fields))
-    message += "\n\n*V5 Core p.216*"
-
-    await ctx.respond(message)
-
-
 _RATINGS = {
-    0: {"surge": 1, "mend": 1, "bonus": 0, "reroll": 0, "severity": 0, "penalty": "None"},
-    1: {"surge": 2, "mend": 1, "bonus": 0, "reroll": 1, "severity": 2, "penalty": "None"},
+    0: {
+        "surge": 1,
+        "mend": 1,
+        "bonus": 0,
+        "reroll": 0,
+        "severity": 0,
+        "penalty": "None",
+        "generations": [14, 16],
+    },
+    1: {
+        "surge": 2,
+        "mend": 1,
+        "bonus": 0,
+        "reroll": 1,
+        "severity": 2,
+        "penalty": "None",
+        "generations": [10, 13],
+    },
     2: {
         "surge": 2,
         "mend": 2,
@@ -77,6 +75,7 @@ _RATINGS = {
         "reroll": 1,
         "severity": 2,
         "penalty": "Animal and bagged blood slake half Hunger",
+        "generations": [8, 13],
     },
     3: {
         "surge": 3,
@@ -85,6 +84,7 @@ _RATINGS = {
         "reroll": 2,
         "severity": 3,
         "penalty": "Animal and bagged blood slake no Hunger",
+        "generations": [6, 13],
     },
     4: {
         "surge": 3,
@@ -93,6 +93,7 @@ _RATINGS = {
         "reroll": 2,
         "severity": 3,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 1 less Hunger per human",
+        "generations": [5, 11],
     },
     5: {
         "surge": 4,
@@ -101,6 +102,7 @@ _RATINGS = {
         "reroll": 3,
         "severity": 4,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 1 less Hunger per human\nMust drain and kill a human to reduce Hunger below 2",
+        "generations": [4, 9],
     },
     6: {
         "surge": 4,
@@ -109,6 +111,7 @@ _RATINGS = {
         "reroll": 3,
         "severity": 4,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 2 less Hunger per human\nMust drain and kill a human to reduce Hunger below 2",
+        "generations": [4, 8],
     },
     7: {
         "surge": 5,
@@ -117,6 +120,7 @@ _RATINGS = {
         "reroll": 4,
         "severity": 5,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 2 less Hunger per human\nMust drain and kill a human to reduce Hunger below 2",
+        "generations": [4, 7],
     },
     8: {
         "surge": 5,
@@ -125,6 +129,7 @@ _RATINGS = {
         "reroll": 4,
         "severity": 5,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 2 less Hunger per human\nMust drain and kill a human to reduce Hunger below 3",
+        "generations": [4, 6],
     },
     9: {
         "surge": 6,
@@ -133,6 +138,7 @@ _RATINGS = {
         "reroll": 5,
         "severity": 6,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 2 less Hunger per human\nMust drain and kill a human to reduce Hunger below 3",
+        "genertations": [4, 5],
     },
     10: {
         "surge": 6,
@@ -141,5 +147,6 @@ _RATINGS = {
         "reroll": 5,
         "severity": 6,
         "penalty": "Animal and bagged blood slake no Hunger\nSlake 3 less Hunger per human\nMust drain and kill a human to reduce Hunger below 3",
+        "generations": [4],
     },
 }

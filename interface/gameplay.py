db2f124833db17126c1eaaf6673f68e06b979f57
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-07-07 16:46:07 -0700

    Add context menu to alter header location

--- interface/gameplay.py
+++ interface/gameplay.py
@@ -8,6 +8,40 @@ from discord.ext import commands
 import inconnu
 
 
+class LocationChangeModal(discord.ui.Modal):
+    """A modal for changing RP header location."""
+
+    def __init__(self, header: discord.Message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.header = header
+
+        self.add_item(
+            discord.ui.InputText(
+                label="New Location",
+                placeholder="The location where the scene takes place",
+                max_length=100,
+            )
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        """Update the RP header."""
+        location = " ".join(self.children[0].value.split())
+        embed = self.header.embeds[0]
+
+        # The title contains name, blush, location, but only name is guaranteed
+        elements = embed.title.split(" • ")
+        if len(elements) > 1 and "Blushed" not in elements[-1]:
+            # We do have location info
+            elements.pop()
+
+        # Add the location
+        elements.append(location)
+        embed.title = " • ".join(elements)
+
+        await self.header.edit(embed=embed)
+        await interaction.response.send_message(f"Location set to **{location}**!", ephemeral=True)
+
+
 async def _header_bol_options(ctx):
     """Generate options for the BoL portion of the header update command."""
     if (charid := ctx.options.get("character")) is None:
@@ -246,6 +280,26 @@ class Gameplay(commands.Cog):
     ):
         """Update your character's RP header."""
         await inconnu.header.update_header(ctx, character, bool(blush))
+
+    @commands.message_command(name="Fix RP Header")
+    @commands.guild_only()
+    async def change_header_location(self, ctx, message: discord.Message):
+        """Change an RP header's location."""
+        if message.author == self.bot.user:
+            # Make sure we have a header
+            record = await inconnu.header_col.find_one({"message": message.id})
+            if record is not None:
+                # Make sure we own it
+                owner = record["character"]["user"]
+                if ctx.channel.permissions_for(ctx.user).administrator or owner == ctx.user.id:
+                    # Modal gets the new location
+                    modal = LocationChangeModal(message, title="Fix RP Header Location")
+                    await ctx.send_modal(modal)
+                else:
+                    await ctx.respond("This isn't your header!", ephemeral=True)
+                return
+
+        await ctx.respond("This message isn't a header!", ephemeral=True)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):

adc124f36e693a1627fc44c7f4caf8dab2333572
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-07-23 14:20:35 -0700

    Change default log level to info

--- config/logging.py
+++ config/logging.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 upload_to_aws = bool(strtobool(os.getenv("AWS_UPLOAD_LOGS", "0")))
-log_level = os.getenv("LOG_LEVEL", "debug")
+log_level = os.getenv("LOG_LEVEL", "info")
 log_enable = int(os.getenv("LOG_ENABLE", "1"))
 log_to_file = bool(strtobool(os.getenv("LOG_TO_FILE", "1")))
 log_dir = os.getenv("LOG_DIR", "logs")

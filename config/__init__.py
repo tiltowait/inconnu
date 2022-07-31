d1b19635e1eccd0e6050931a63d6a1e9e484e51d
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-07-31 11:48:15 -0700

    Stop linter from screaming

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,7 @@
 """Basic bot config."""
 
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 
@@ -8,8 +9,8 @@ from logger import Logger
 
 load_dotenv()
 
+DEBUG_GUILDS: Optional[list] = None
+
 if (_debug_guilds := os.getenv("DEBUG")) is not None:
     DEBUG_GUILDS = [int(g) for g in _debug_guilds.split(",")]
     Logger.info("MAIN: Debugging on %s", DEBUG_GUILDS)
-else:
-    DEBUG_GUILDS = None

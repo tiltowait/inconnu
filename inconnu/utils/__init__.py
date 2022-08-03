33405f9f408544892bce17a58215923afe2dcdce
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-03 15:29:49 -0700

    Add raw_command_options

--- inconnu/utils/__init__.py
+++ inconnu/utils/__init__.py
@@ -5,6 +5,17 @@ from .haven import Haven
 from .paramparse import parse_parameters
 
 
+def raw_command_options(interaction) -> str:
+    """Get the options in a command as a dict."""
+    options = {}
+    for option in interaction.data.get("options", []):
+        name = option["name"]
+        value = option["value"]
+        options[name] = value
+
+    return options
+
+
 def command_options(interaction) -> str:
     """Format the command options for easy display."""
     options = []

0a25d4e2afbbb6084cd6ce974a4ddcca24ad7487
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-03 14:58:44 -0700

    Merge convictions show and set

--- interface/characters.py
+++ interface/characters.py
@@ -230,28 +230,22 @@ class Characters(commands.Cog, name="Character Management"):
 
     # Convictions
 
-    convictions = character.create_subgroup("convictions", "Character convictions")
-
-    @convictions.command(name="set")
+    @character.command(name="convictions")
     @commands.guild_only()
-    async def character_convictions_set(
+    async def character_convictions(
         self,
         ctx: discord.ApplicationContext,
-        character: inconnu.options.character("The character to look up", required=True),
-    ):
-        """Change or view Convictions."""
-        await inconnu.character.convictions_set(ctx, character)
-
-    @convictions.command(name="show")
-    @commands.guild_only()
-    async def character_convictions_show(
-        self,
-        ctx: discord.ApplicationContext,
-        character: inconnu.options.character("The character to look up", required=False),
-        player: inconnu.options.player,
+        character: inconnu.options.character("The character to show"),
+        player: Option(
+            discord.Member, "The character's owner (does not work with EDIT)", required=False
+        ),
+        edit: inconnu.options.character("The character whose convictions to set"),
     ):
         """Show a character's Convictions."""
-        await inconnu.character.convictions_show(ctx, character, player, False)
+        if edit is None:
+            await inconnu.character.convictions_show(ctx, character, player, False)
+        else:
+            await inconnu.character.convictions_set(ctx, edit)
 
     @commands.user_command(name="Convictions")
     @commands.guild_only()

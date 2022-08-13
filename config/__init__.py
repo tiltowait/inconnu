ec2d09c9bc74124b1d3d312839d6c07dc00f5929
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-13 11:16:42 -0700

    Add AWS path generator

--- config/__init__.py
+++ config/__init__.py
@@ -14,3 +14,11 @@ DEBUG_GUILDS: Optional[list] = None
 if (_debug_guilds := os.getenv("DEBUG")) is not None:
     DEBUG_GUILDS = [int(g) for g in _debug_guilds.split(",")]
     Logger.info("MAIN: Debugging on %s", DEBUG_GUILDS)
+
+
+def aws_asset(path: str):
+    """Returns the AWS URL for the given path."""
+    base = "https://inconnu.s3-us-west-1.amazonaws.com/assets/"
+    if path[0] == "/":
+        path = path[1:]
+    return base + path

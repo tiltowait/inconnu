37e832072d9ce7eadbe9014b74603db81227016b
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-21 09:01:54 -0700

    Move to Paginator subclass

--- inconnu/character/images/display.py
+++ inconnu/character/images/display.py
@@ -31,27 +31,7 @@ async def display_images(
     )
     character = await haven.fetch()
 
-    pages_ = []
-    for index, image in enumerate(character.image_urls):
-        Logger.debug("IMAGES: (%s) Making view for %s", character.name, image)
-        embed = inconnu.utils.VCharEmbed(ctx, character, haven.owner, show_thumbnail=False)
-        embed.set_thumbnail(url=ctx.guild.icon)
-        embed.set_image(url=image)
-        embed.set_footer(
-            text=(
-                "Images are a premium feature. Become a supporter with /patreon!\n"
-                "Users are solely responsible for image content."
-            )
-        )
-        pages_.append(
-            pages.Page(
-                embeds=[embed],
-                custom_view=_DeleteImageView(character, index),
-            )
-        )
-
-    Logger.info("IMAGES: Found %s images for %s", len(pages_), character.name)
-    paginator = pages.Paginator(pages_, loop_pages=True)
+    paginator = ImagePaginator(ctx, character, haven.owner)
     await paginator.respond(ctx.interaction)
 
 
@@ -61,6 +41,90 @@ def _has_image(character):
         if image:
             return
     raise inconnu.errors.CharacterError(f"{character.name} has no images set!")
+
+
+class ImagePaginator(pages.Paginator):
+    """A Paginator for displaying character images."""
+
+    def __init__(self, ctx, character, owner):
+        self.ctx = ctx
+        self.character = character
+        self.owner = owner
+        self.embeds = self._generate_pages()
+
+        super().__init__(
+            self.embeds,
+            loop_pages=True,
+            author_check=False,
+            custom_view=_DeleteImager(character.user, self._delete_image),
+        )
+
+    async def _delete_image(self, interaction: discord.Interaction):
+        """Delete the image."""
+        del self.embeds[self.current_page]
+        if self.embeds:
+            await self.update(
+                pages=self.embeds,
+                loop_pages=True,
+                author_check=False,
+                custom_view=self.custom_view,
+                interaction=interaction,
+            )
+        else:
+            embed = inconnu.utils.VCharEmbed(
+                self.ctx,
+                self.character,
+                self.owner,
+                title="No Images",
+                description=f"**{self.character.name}** has no images!",
+                show_thumbnail=False,
+            )
+            embed.set_footer(text="Upload some with /character image upload.")
+
+            await interaction.response.edit_message(embed=embed, view=None)
+            self.stop()
+            self.custom_view.stop()
+
+    def _generate_pages(self):
+        """Generate the pages."""
+        pages_ = []
+        for image in self.character.image_urls:
+            Logger.debug("IMAGES: (%s) Making view for %s", self.character.name, image)
+            embed = inconnu.utils.VCharEmbed(
+                self.ctx, self.character, self.owner, show_thumbnail=False
+            )
+            # embed.set_thumbnail(url=embed.author.icon_url)
+            embed.set_image(url=image)
+            embed.set_footer(
+                text=(
+                    "Images are a premium feature. Become a supporter with /patreon!\n"
+                    "Users are solely responsible for image content."
+                )
+            )
+            pages_.append(embed)
+
+        Logger.info("IMAGES: Found %s images for %s", len(pages_), self.character.name)
+        return pages_
+
+
+class _DeleteImager(ReportingView):
+    """A View that tells its parent to delete images."""
+
+    def __init__(self, owner, callback, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.owner = owner
+        self.callback = callback
+
+    @discord.ui.button(label="Delete Image", style=discord.ButtonStyle.danger)
+    async def delete_image(self, _, interaction: discord.Interaction):
+        """Inform the callback of the deletion event."""
+        if interaction.user.id != self.owner:
+            await interaction.response.send_message(
+                "This character doesn't belong to you!",
+                ephemeral=True,
+            )
+        else:
+            await self.callback(interaction)
 
 
 class _DeleteImageView(ReportingView):
@@ -93,10 +157,3 @@ class _DeleteImageView(ReportingView):
         await interaction.response.edit_message(embed=embed, view=None)
         await s3.delete_file(self.image_url)
         self.stop()
-
-    async def interaction_check(self, interaction: discord.Interaction):
-        """Ensure only the character's owner can click the button."""
-        if self.character.user != interaction.user.id:
-            await interaction.response.send_message("You don't own this button!", ephemeral=True)
-            return False
-        return True

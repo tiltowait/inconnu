526e9aa164709b886a038d8859ec4be48e588d82
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-21 23:15:41 -0700

    Display admin guild

--- config/__init__.py
+++ config/__init__.py
@@ -12,6 +12,8 @@ load_dotenv()
 DEBUG_GUILDS: Optional[list] = None
 ADMIN_GUILD = int(os.environ["ADMIN_SERVER"])
 
+Logger.info("CONFIG: Admin guild: %s", ADMIN_GUILD)
+
 if (_debug_guilds := os.getenv("DEBUG")) is not None:
     DEBUG_GUILDS = [int(g) for g in _debug_guilds.split(",")]
     Logger.info("MAIN: Debugging on %s", DEBUG_GUILDS)

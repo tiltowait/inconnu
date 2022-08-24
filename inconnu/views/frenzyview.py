4b8e12398338b479d9ee0407e158655eb387a189
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-23 22:49:29 -0700

    Frenzy button turns gray when used

--- inconnu/views/frenzyview.py
+++ inconnu/views/frenzyview.py
@@ -1,7 +1,6 @@
 """views/frenzyview.py - A view that rolls frenzy."""
 
-from discord import ButtonStyle
-from discord.ui import Button
+import discord
 
 import inconnu
 
@@ -16,19 +15,23 @@ class FrenzyView(DisablingView):
         self.character = character
         self.difficulty = difficulty
 
-        frenzy = Button(label=f"Hunger Frenzy (DC {difficulty})", style=ButtonStyle.danger)
-        frenzy.callback = self.frenzy
-        self.add_item(frenzy)
+        self.frenzy_button = discord.ui.Button(
+            label=f"Hunger Frenzy (DC {difficulty})", style=discord.ButtonStyle.danger
+        )
+        self.frenzy_button.callback = self.frenzy
+        self.add_item(self.frenzy_button)
 
     async def frenzy(self, interaction):
         """Frenzy, if applicable."""
+        self.frenzy_button.style = discord.ButtonStyle.secondary
+        await self.disable_items(interaction)
+        await inconnu.misc.frenzy(interaction, self.difficulty, None, None, self.character)
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        """Check that the user is the character's owner."""
         if interaction.user.id == self.character.user:
-            await self.disable_items(interaction)
-            await inconnu.misc.frenzy(interaction, self.difficulty, None, None, self.character)
-        else:
-            if interaction.response.is_done():
-                await interaction.followup.send("You can't click this button.", ephemeral=True)
-            else:
-                await interaction.response.send_message(
-                    "You can't click this button.", ephemeral=True
-                )
+            return True
+        await interaction.response.send_message(
+            "This button doesn't belong to you!", ephemeral=True
+        )
+        return False

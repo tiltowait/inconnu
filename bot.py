39e8e5231cd35e3e58d5980135d57d308ea0a066
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-30 16:56:59 -0700

    Only check premium status on the first of the month

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@
 
 import asyncio
 import os
-from datetime import time, timezone
+from datetime import datetime, time, timezone
 
 import discord
 from discord.ext import tasks
@@ -311,11 +311,14 @@ async def cull_inactive():
     await inconnu.tasks.cull()
 
 
-@tasks.loop(minutes=10)
-# @tasks.loop(time=time(12, 0, tzinfo=timezone.utc))
+@tasks.loop(time=time(0, tzinfo=timezone.utc))
 async def check_premium_expiries():
     """Perform required actions on expired premium users."""
-    await inconnu.tasks.premium.remove_expired_images()
+    if discord.utils.utcnow().day == 1:
+        # Only run task on the first day of the month
+        await inconnu.tasks.premium.remove_expired_images()
+    else:
+        Logger.debug("TASK: Premium expiration checks only happen on the first of the month")
 
 
 @tasks.loop(hours=1)

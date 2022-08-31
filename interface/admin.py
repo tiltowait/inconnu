999c24647d4c6336d6348e79d6c1a77f317fcd05
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-30 19:39:54 -0700

    Rename the MOTD commands

--- interface/admin.py
+++ interface/admin.py
@@ -16,12 +16,10 @@ from logger import Logger
 class AdminCog(commands.Cog):
     """A cog with various administrative commands."""
 
-    motd = SlashCommandGroup("motd", "Message of the Day commands")
-
-    @motd.command(name="set", guild_ids=[ADMIN_GUILD])
+    @discord.slash_command(guild_ids=[ADMIN_GUILD])
     @discord.default_permissions(administrator=True)
     @commands.is_owner()
-    async def motd_set(
+    async def announce(
         self,
         ctx: discord.ApplicationContext,
         title: Option(str, "The message's title"),
@@ -41,7 +39,7 @@ class AdminCog(commands.Cog):
 
         embed = discord.Embed(title=title, description=description)
         embed.set_author(name=ctx.bot.user.display_name, icon_url=ctx.bot.user.display_avatar)
-        embed.set_footer(text="This is a one-time message. To see it again, use /motd show.")
+        embed.set_footer(text="This is a one-time message. To see it again, use /motd.")
 
         try:
             for field, value in [(field1_name, field1_value), (field2_name, field2_value)]:
@@ -55,21 +53,21 @@ class AdminCog(commands.Cog):
 
             # Set the MotD
             ctx.bot.set_motd(embed)
-            await ctx.respond("Message of the Day set!", embed=embed, ephemeral=True)
+            await ctx.respond("Message of the Day set!", ephemeral=True)
 
         except AttributeError:
             await ctx.respond("A field must have an associated value!", ephemeral=True)
 
-    @motd.command(name="unset", guild_ids=[ADMIN_GUILD])
+    @discord.slash_command(guild_ids=[ADMIN_GUILD])
     @discord.default_permissions(administrator=True)
     @commands.is_owner()
-    async def motd_unset(self, ctx: discord.ApplicationContext):
+    async def unannounce(self, ctx: discord.ApplicationContext):
         """Unset the Message of the Day."""
         ctx.bot.set_motd(None)
         await ctx.respond("Message of the Day unset!", ephemeral=True)
 
-    @motd.command(name="show")
-    async def motd_show(self, ctx: discord.ApplicationContext):
+    @discord.slash_command()
+    async def motd(self, ctx: discord.ApplicationContext):
         """Show the Message of the Day."""
         if ctx.bot.motd is not None:
             await ctx.respond(embed=ctx.bot.motd, ephemeral=True)

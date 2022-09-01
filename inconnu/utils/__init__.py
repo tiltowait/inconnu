7dfc368131c0632bc9b4faac607ff091f22a41ec
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-08-31 17:06:13 -0700

    Remove debug statements

--- inconnu/utils/__init__.py
+++ inconnu/utils/__init__.py
@@ -6,7 +6,6 @@ from discord.ext import commands
 import inconnu
 from config import SUPPORTER_GUILD, SUPPORTER_ROLE
 from inconnu import errors
-from logger import Logger
 
 from .decorators import not_on_lockdown
 from .error import ErrorEmbed, error
@@ -56,15 +55,9 @@ def is_supporter(ctx, user: discord.Member = None) -> bool:
 
     # First, see if the invoker is on the support server
     if (member := support_server.get_member(user.id)) is not None:
-        Logger.debug(
-            "SUPPORTER: %s#%s is on %s", member.name, member.discriminator, support_server.name
-        )
         if member.get_role(SUPPORTER_ROLE) is not None:
-            Logger.info("SUPPORTER: %s#%s is a supporter", member.name, member.discriminator)
             return True
-        Logger.info("SUPPORTER: %s#%s is a not a supporter", member.name, member.discriminator)
         return False
-    Logger.debug("SUPPORTER: %s#%s is not on the support server", member.name, member.discriminator)
     return False
 
 

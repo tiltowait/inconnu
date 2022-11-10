40359b74ead6d3f1629e003cd7f8feb5a7c930a3
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-11-10 14:58:52 -0800

    Pretty up search embed a bit

--- inconnu/roleplay/search.py
+++ inconnu/roleplay/search.py
@@ -23,9 +23,14 @@ async def search(ctx, user: discord.Member, needle: str, charid: ObjectId = None
     async for post in RPPost.find(query).sort("content", {"$meta": "textScore"}).limit(5):
         # Make an embed for each post
         embed = discord.Embed(
-            title=f"Post #{num}: {post.header.char_name}", description=post.content, url=post.url
+            title=f"Post #{num}: {post.header.char_name}",
+            description=post.content,
+            url=post.url,
+            timestamp=post.utc_date,
         )
         embed.set_footer(text=f"Search key: {needle}")
+        embed.set_author(name=user.display_name, icon_url=user.display_avatar)
+
         posts.append(embed)
         num += 1
 

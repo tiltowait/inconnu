f33d0f0975e465ed542644e862f81e0cb7a463b5
tiltowait <208040+tiltowait@users.noreply.github.com>
2022-12-22 16:08:56 -0800

    Speedup image uploads by via API

--- inconnu/character/images/upload.py
+++ inconnu/character/images/upload.py
@@ -1,16 +1,12 @@
 """Character image uploading."""
 
 import os
-from pathlib import Path
 
 import aiohttp
 import async_timeout
 import discord
-from bson.objectid import ObjectId
-from PIL import Image
 
 import inconnu
-import s3
 from logger import Logger
 
 __HELP_URL = "https://docs.inconnu.app/guides/premium/character-images"
@@ -41,47 +37,39 @@ async def upload_image(ctx: discord.ApplicationContext, image: discord.Attachmen
         # defer if we've responded, so ...
         await ctx.defer(ephemeral=True)
 
-    original = await download_file(image.url)
-    Logger.debug(
-        "IMAGES: Downloaded image for %s, size=%s", character.name, os.stat(original).st_size
-    )
-    webp = convert_to_webp(original)
+    try:
+        aws_image_url = await process_file(character.id, image.url)
+        Logger.info("IMAGES: %s: Uploaded new image to %s", character.name, aws_image_url)
 
-    destination = f"profiles/{character.id}/{webp}"
-    await s3.upload_file(webp, destination)
-    aws_url = s3.get_url(destination)
+        character.profile.images.append(aws_image_url)
 
-    if aws_url in character.profile.images:
-        Logger.info("IMAGES: %s: Ignoring duplicate image: %s", character.name, aws_url)
-    else:
-        Logger.info("IMAGES: %s: Adding image URL: %s", character.name, aws_url)
-        character.profile.images.append(aws_url)
-    delete_file(webp)
+        embed = inconnu.utils.VCharEmbed(
+            ctx,
+            character,
+            title="Image uploaded!",
+            show_thumbnail=False,
+        )
+        embed.set_image(url=aws_image_url)
+        embed.set_footer(text="View your images with /character images.")
 
-    embed = inconnu.utils.VCharEmbed(
-        ctx,
-        character,
-        title="Image uploaded!",
-        show_thumbnail=False,
-    )
-    embed.set_image(url=aws_url)
-    embed.set_footer(text="View your images with /character images.")
+        await ctx.respond(embed=embed, ephemeral=True)
+        await character.commit()
 
-    await ctx.respond(embed=embed, ephemeral=True)
-    await character.commit()
+        # We maintain a log of all image uploads to protect ourself against
+        # potential legal claims if someone uploads something illegal
+        await inconnu.db.upload_log.insert_one(
+            {
+                "guild": ctx.guild.id,
+                "user": ctx.user.id,
+                "charid": character.pk,
+                "url": aws_image_url,
+                "deleted": False,
+                "timestamp": discord.utils.utcnow(),
+            }
+        )
 
-    # We maintain a log of all image uploads to protect ourself against
-    # potential legal claims if someone uploads something illegal
-    await inconnu.db.upload_log.insert_one(
-        {
-            "guild": ctx.guild.id,
-            "user": ctx.user.id,
-            "charid": character.pk,
-            "url": aws_url,
-            "deleted": False,
-            "timestamp": discord.utils.utcnow(),
-        }
-    )
+    except (aiohttp.ClientResponseError, aiohttp.ClientConnectorError):
+        await inconnu.utils.error(ctx, "Unable to process image. Please try again later.")
 
 
 def valid_url(url: str) -> bool:
@@ -95,40 +83,15 @@ def valid_url(url: str) -> bool:
     return False
 
 
-async def download_file(url: str) -> Path:
-    """Download a file."""
-    filename = Path(str(ObjectId()))
-    Logger.debug("IMAGES: Downloading %s to %s", url, filename)
+async def process_file(charid: str, image_url: str) -> str:
+    """Send the file URL to the API for processing."""
+    url = "https://api.inconnu.app/upload-fc"
+    token = os.environ["INCONNU_API_TOKEN"]
+    header = {"Authorization": f"token {token}"}
+    payload = {"charid": charid, "image_url": image_url}
 
-    async with async_timeout.timeout(120):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                with open(filename, "wb") as file:
-                    async for data in response.content.iter_chunked(1024):
-                        file.write(data)
-
-    return filename
-
-
-def convert_to_webp(source: str, quality=99) -> Path:
-    """Convert an image file to WebP."""
-    destination = source.with_suffix(".webp")
-    image = Image.open(source)
-    image.save(destination, format="webp", quality=quality)
-    Logger.debug(
-        "IMAGES: Converted %s to WebP with quality=%s, size=%s",
-        source,
-        quality,
-        os.stat(destination).st_size,
-    )
-    delete_file(source)
-    return destination
-
-
-def delete_file(file: str):
-    """Delete a file."""
-    if os.path.exists(file):
-        Logger.debug("IMAGES: Deleting temporary image file %s", file)
-        os.remove(file)
-    else:
-        Logger.error("IMAGES: Attempted to delete file that doesn't exist: %s", file)
+    async with async_timeout.timeout(60):
+        async with aiohttp.ClientSession(headers=header, raise_for_status=True) as session:
+            async with session.post(url, json=payload) as response:
+                response_json = await response.json()
+                return response_json["url"]  # The image's new bucket URL

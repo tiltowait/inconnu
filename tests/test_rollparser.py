2658c12fe555fc26eaafe6edca90665d70ec7bb7
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-02-14 16:11:25 -0800

    Test HungerInPool

--- tests/test_rollparser.py
+++ tests/test_rollparser.py
@@ -2,6 +2,7 @@
 
 import pytest
 
+import inconnu.errors
 from inconnu.models.vchar import VChar
 from inconnu.vr.rollparser import RollParser
 from tests.characters import gen_char
@@ -54,3 +55,26 @@ def test_pool_creation(syntax: str, pool_str: str, value: int, character: VChar)
 def test_stack_expansion(syntax: str, stack: str, expand_only: bool, character: VChar):
     p = RollParser(character, syntax, expand_only)
     assert p.pool_stack == stack
+
+
+@pytest.mark.parametrize(
+    "syntax,should_fail",
+    [
+        ("stren+br", False),
+        ("1+2", False),
+        ("strength + brawl hunger", False),
+        ("strength + brawl current_hunger", False),
+        ("stren+hung", True),
+        ("strength + hunger", True),
+        ("strength + brawl + current_hunger", True),
+    ],
+)
+def test_hunger_in_pool(syntax: str, should_fail: bool, character: VChar):
+    if should_fail:
+        with pytest.raises(inconnu.errors.HungerInPool):
+            _ = RollParser(character, syntax)
+    else:
+        try:
+            _ = RollParser(character, syntax)
+        except inconnu.errors.HungerInPool:
+            pytest.fail(msg="Should not have raised HungerInPool")

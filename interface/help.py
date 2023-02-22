7c77b67a4c47d0aa2bfd81dff3862a9b06e025e9
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-02-21 16:09:20 -0800

    Revamp help commands

--- interface/help.py
+++ interface/help.py
@@ -1,13 +1,33 @@
 """interface/help.py - Help commands"""
+# TODO: Migrate to _empty_embed
+
+from enum import Enum
 
 import discord
-from discord.commands import SlashCommandGroup, slash_command
+from discord import option
+from discord.commands import slash_command
 from discord.ext import commands
 from discord.ui import Button, View
 
 import inconnu
 from config import web_asset
 from inconnu.constants import PATREON, SUPPORT_URL
+
+
+class Section(str, Enum):
+    """Represents a help section."""
+
+    OVERVIEW = "Overview"
+    CHARACTERS = "Characters"
+    TRAITS = "Traits"
+    DISCIPLINES = "Disciplines"
+    SPECIALTIES = "Specialties"
+    MACROS = "Macros"
+
+    @classmethod
+    def all(cls) -> list[str]:
+        """A list of all values."""
+        return [section.value for section in cls]
 
 
 class _HelpView(View):
@@ -70,14 +90,23 @@ class Help(commands.Cog):
             )
             traits.callback = self.show_traits_help
 
+            specialties = Button(
+                label="Specialties",
+                style=discord.ButtonStyle.primary,
+                custom_id="persistent-specialties",
+            )
+            specialties.callback = self.show_specialties_help
+
             macros = Button(
-                label="Macro Management",
+                label="Macros",
                 style=discord.ButtonStyle.primary,
                 custom_id="persistent-macro",
             )
             macros.callback = self.show_macros_help
 
-            self.overview_view = _HelpView(character, traits, macros, show_support=True)
+            self.overview_view = _HelpView(
+                character, traits, specialties, macros, show_support=True
+            )
 
         # Add the persistent views to the bot if we haven't already
         if not self.bot.persistent_views_added:
@@ -87,30 +116,26 @@ class Help(commands.Cog):
 
     # Help Commands
 
-    help_commands = SlashCommandGroup("help", "Help commands.")
-
-    @help_commands.command(name="overview")
-    async def help_overview(self, ctx):
-        """Basic usage instructions."""
-        await self.show_basic_help(ctx, False)
-
-    @help_commands.command(name="traits")
-    @commands.guild_only()
-    async def help_traits(self, ctx):
-        """Trait management instructions."""
-        await self.show_traits_help(ctx, False)
-
-    @help_commands.command(name="macros")
-    @commands.guild_only()
-    async def help_macros(self, ctx):
-        """Macro usage instructions."""
-        await self.show_macros_help(ctx, False)
-
-    @help_commands.command(name="characters")
-    @commands.guild_only()
-    async def help_character_updates(self, ctx):
-        """Display the valid character update keys."""
-        await inconnu.character.update_help(ctx, ephemeral=False)
+    # TODO: "help"
+    @slash_command(name="help")
+    @option("section", description="The help section to look up", choices=Section.all())
+    async def help_command(self, ctx: discord.ApplicationContext, section: str):
+        """View help on a particular section."""
+        match section:
+            case Section.OVERVIEW:
+                await self.show_basic_help(ctx, False)
+            case Section.CHARACTERS:
+                await inconnu.character.update_help(ctx, ephemeral=False)
+            case Section.TRAITS:
+                await self.show_traits_help(ctx, False)
+            case Section.DISCIPLINES:
+                await self.show_disciplines_help(ctx, False)
+            case Section.SPECIALTIES:
+                await self.show_specialties_help(ctx, False)
+            case Section.MACROS:
+                await self.show_macros_help(ctx, False)
+            case _:
+                raise NotImplementedError("Oops")
 
     @slash_command()
     async def info(self, ctx):
@@ -122,14 +147,6 @@ class Help(commands.Cog):
         embed.set_author(name=self.bot.user.display_name, icon_url=self.bot.user.avatar)
         embed.set_thumbnail(url=web_asset("darkpack_logo2.webp"))
 
-        help_commands = [
-            "`/help overview` - Basic help",
-            "`/help characters` - Character updates help",
-            "`/help traits` - Trait management help",
-            "`/help macros` - Macro creation/editing help",
-        ]
-
-        embed.add_field(name="Help Commands", value="\n".join(help_commands), inline=False)
         embed.add_field(
             name="Framework", value="[Pycord 2.3](https://pycord.dev) using Python 3.10"
         )
@@ -140,7 +157,12 @@ class Help(commands.Cog):
         embed.add_field(name="Author", value="@tiltowait#8282", inline=False)
 
         embed.set_footer(
-            text="Portions of the materials are the copyrights and trademarks of Paradox Interactive AB, and are used with permission. All rights reserved. For more information please visit worldofdarkness.com."
+            text=(
+                "Portions of the materials are the copyrights and trademarks "
+                "of Paradox Interactive AB, and are used with permission. All "
+                "rights reserved. For more information please visit "
+                "worldofdarkness.com."
+            )
         )
 
         await inconnu.utils.cmd_replace(ctx, embed=embed, view=self.info_view)
@@ -231,31 +253,135 @@ class Help(commands.Cog):
         )
         embed.set_author(name=self.bot.user.display_name)
         embed.set_thumbnail(url=self.bot.user.avatar)
-        embed.set_footer(text="Traits may be used in rolls. See /help overview for more info.")
 
         embed.add_field(
-            name="Addition",
-            value="`/traits add`\n**Example:** `/traits add traits:Oblivion=3 Chemistry=1`",
+            name="Add custom traits",
+            value="`/traits add`\n**Example:** `/traits add traits:Forgery=1 Stunning=2`",
             inline=False,
         )
         embed.add_field(
-            name="Deletion",
-            value="`/traits delete`\n**Example:** `/traits delete traits:Oblivion`",
+            name="Delete custom traits",
+            value="`/traits delete`\n**Example:** `/traits delete traits:Forgery`",
             inline=False,
         )
         embed.add_field(
-            name="Modification",
-            value="`/traits update`\n**Example:** `/traits update traits:Oblivion=2`",
+            name="Modify traits",
+            value="`/traits update`\n**Example:** `/traits update traits:Strength=2 Brawl=3`",
             inline=False,
         )
         embed.add_field(
-            name="Specialties",
-            value="`/specialties add`\n**Example:** `/specialties add specialties:Grappling`",
+            name="Disciplines, Specialties, & Powers",
+            value="See the respective sections of this help command.",
+            inline=False,
+        )
+        embed.add_field(name="List traits", value="`/traits list`", inline=False)
+        embed.add_field(
+            name="Roll with a trait",
+            value="**Example:** `/roll pool:Strength + Brawl hunger:Current Hunger difficulty:1`",
             inline=False,
         )
 
         buttons = [
             Button(label="Documentation", url="https://docs.inconnu.app/command-reference/traits"),
+            Button(label="Support", url=SUPPORT_URL),
+        ]
+        view = _HelpView(*buttons)
+
+        await inconnu.utils.cmd_replace(ctx, embed=embed, view=view, ephemeral=ephemeral)
+
+    async def show_specialties_help(self, ctx, ephemeral=True):
+        """Show help for using/adding/removing specialties."""
+        embed = self._empty_embed(
+            title="Specialties",
+            description="With these commands, you may add or remove skill specialties.",
+        )
+        embed.add_field(
+            name="Add specialties",
+            value=(
+                "`/specialties add` - Add specialties to standard or custom skills.\n"
+                "**Example:** `/specialties add specialties:Performance=Piano,Singing Craft=Ammo`"
+            ),
+            inline=False,
+        )
+        embed.add_field(
+            name="Remove specialties",
+            value=(
+                "`/specialties remove` - Remove specialties from standard or custom skills.\n"
+                "**Example:** `/specialties remove specialties:Brawl=Kindred`"
+            ),
+            inline=False,
+        )
+        embed.add_field(name="List specialties", value="`/traits list`")
+        embed.add_field(
+            name="Roll specialty",
+            value=(
+                "**Example:** "
+                "`/roll pool:Strength + Brawl.Kindred hunger:Current Hunger difficulty:1`"
+            ),
+            inline=False,
+        )
+        embed.set_footer(text="You may add and remove multiple specialties at a time.")
+
+        # TODO: Correct URL
+        buttons = [
+            Button(label="Documentation", url="https://docs.inconnu.app/command-reference/macros"),
+            Button(label="Support", url=SUPPORT_URL),
+        ]
+        view = _HelpView(*buttons)
+
+        await inconnu.utils.cmd_replace(ctx, embed=embed, view=view, ephemeral=ephemeral)
+
+    async def show_disciplines_help(self, ctx, ephemeral=True):
+        """Show help for using/adding/removing specialties."""
+        embed = self._empty_embed(
+            title="Disciplines & Powers",
+            description="With these commands, you may add or remove Disciplines and powers.",
+        )
+        embed.add_field(
+            name="Add Disciplines",
+            value=(
+                "`/disciplines add` - Add one or more Disciplines and their ratings.\n"
+                "**Example:** `/disciplines add:Auspex=2 Oblivion=3`"
+            ),
+            inline=False,
+        )
+        embed.add_field(
+            name="Remove Disciplines",
+            value=(
+                "`/disciplines remove` - Remove one or more Disciplines.\n"
+                "**Example:** `/disciplines remove:Oblivion Auspex`"
+            ),
+            inline=False,
+        )
+        embed.add_field(
+            name="Add Powers",
+            value=(
+                "`/powers add` - Add one or more powers to Disciplines.\n"
+                "**Example:** `/powers add powers:Potence=Prowess,SoaringLeap`"
+            ),
+            inline=False,
+        )
+        embed.add_field(
+            name="Remove Powers",
+            value=(
+                "`/powers remove` - Remove one or more powers to Disciplines.\n"
+                "**Example:** `/powers remove powers:Potence=Prowess Auspex=Telepathy`"
+            ),
+            inline=False,
+        )
+        embed.add_field(name="List Disciplines & Powers", value="`/traits list`")
+        embed.add_field(
+            name="Roll Discipline with Power",
+            value=(
+                "**Example:** "
+                "`/roll pool:Resolve + Auspex.Telepathy hunger:Current Hunger difficulty:3`"
+            ),
+            inline=False,
+        )
+
+        # TODO: Correct URL
+        buttons = [
+            Button(label="Documentation", url="https://docs.inconnu.app/command-reference/macros"),
             Button(label="Support", url=SUPPORT_URL),
         ]
         view = _HelpView(*buttons)
@@ -300,6 +426,14 @@ class Help(commands.Cog):
     async def show_character_help(self, ctx, ephemeral=True):
         """Run the /macro help command."""
         await inconnu.character.update_help(ctx, ephemeral=ephemeral)
+
+    def _empty_embed(self, *, title: str, description: str):
+        """Create an empty help embed."""
+        embed = discord.Embed(title=title, description=description)
+        embed.set_author(name=self.bot.user.display_name)
+        embed.set_thumbnail(url=self.bot.user.avatar)
+
+        return embed
 
 
 def setup(bot):

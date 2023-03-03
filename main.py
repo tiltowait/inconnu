90a440d8ebb8b5a165f949cb656f1978a78c6fad
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-03-03 14:41:03 -0800

    Use jinja2

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ Invite: https://discord.com/api/oauth2/authorize?client_id=882409882119196704&pe
 import asyncio
 import json
 import os
-from typing import Dict, List
 
 import discord
-import markdown
 from bson.objectid import ObjectId
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.exceptions import HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from starlette import status
 
 import bot
@@ -24,6 +23,8 @@ from logger import Logger
 
 app = FastAPI(openapi_url=None)
 app.mount("/web", StaticFiles(directory="web"), name="web")
+
+templates = Jinja2Templates(directory="web/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -38,17 +39,15 @@ async def home():
 if SHOW_TEST_ROUTES:
 
     @app.get("/test", response_class=HTMLResponse)
-    async def offline_page():
+    async def offline_page(request: Request):
         """Generate an offline test page."""
         with open("web/snippets.json", "r", encoding="utf-8") as file:
             profile = json.load(file)["sample"]
-            profile["user"] = 0
-            profile["guild"] = 0
-            return prepare_html(profile)
+            return prepare_html(request, profile)
 
 
 @app.get("/profile/{charid}", response_class=HTMLResponse)
-async def display_character_profile(charid: str):
+async def display_character_profile(request: Request, charid: str):
     """Display character biography detail."""
     if not ObjectId.is_valid(charid):
         raise HTTPException(400, detail="Improper character ID.")
@@ -62,94 +61,30 @@ async def display_character_profile(charid: str):
         raise HTTPException(404, detail="Character not found.")
 
     # Got the character; return the HTML
-    return prepare_html(bio)
+    return prepare_html(request, bio)
 
 
-def prepare_html(bio: Dict[str, str | dict[str, str]]) -> str:
+def prepare_html(request: Request, bio: dict[str, str | dict[str, str]]) -> str:
     """Prep the character HTML page."""
-    with open("web/snippets.json", "r", encoding="utf-8") as snippets:
-        snippets = json.load(snippets)
+    name = bio["name"]
+    profile = bio.get("profile", {})
 
-        # Basic profile info
-        name = bio["name"]
-        profile = bio.get("profile", {})
-        biography = markdown.markdown(profile.get("biography", "")) or snippets["unset"]
-        description = markdown.markdown(profile.get("description", "")) or snippets["unset"]
+    # Ownership data
+    guild = bot.bot.get_guild(bio["guild"])
+    user = guild.get_member(bio["user"]) if guild is not None else None
 
-        # Get the profile image by template
-        image_prop, image = generate_image_column(snippets, name, profile.get("images", []))
-
-        # Generate the ownership string and icons
-        guild = bot.bot.get_guild(bio["guild"])
-        user = guild.get_member(bio["user"]) if guild is not None else None
-        icons = get_icons(snippets, user, guild)
-
-        if guild is None:
-            ownership = snippets["unknown"]
-        elif user is None:
-            ownership = snippets["unowned"].format(guild=guild.name)
-        elif user == bot.bot.user:
-            ownership = snippets["spc"].format(guild=guild.name)
-        else:
-            # Guild found, user found, user not the bot
-            ownership = snippets["owned"].format(user=user.name, guild=guild.name)
-
-    with open("web/profile.html", "r", encoding="utf-8") as html_file:
-        html = html_file.read()
-        return html.format(
-            name=name,
-            url=inconnu.profile_url(bio["_id"]),
-            image_prop=image_prop,
-            biography=biography,
-            description=description,
-            image=image,
-            ownership=ownership,
-            icons="\n".join(icons),
-        )
-
-
-def get_icons(snippets, user, guild) -> List[str]:
-    """Get the icons for the user and guild."""
-    icons = []
-    if user is not None:
-        icons.append(
-            snippets["icon"].format(source=inconnu.get_avatar(user), name=user.display_name)
-        )
-    if guild is not None:
-        icons.append(snippets["icon"].format(source=guild.icon, name=guild.name))
-
-    return icons
-
-
-def generate_image_column(snippets, name, images):
-    """Generate the HTML for the image column."""
-    if not images:
-        image_prop = ""
-        image = snippets["no_profile_image"]
-    else:
-        image_prop = snippets["image_prop"].format(source=images[0])
-        carousel_items = []
-        for index, image in enumerate(images):
-            section_classes = "carousel-item"
-            if index == 0:
-                section_classes += " active"
-            carousel_items.append(
-                f"""
-                <div class="{section_classes}">
-                  <img src="{image}" class="d-block w-100" alt="{name}">
-                </div>
-                """
-            )
-        items = "\n".join(carousel_items)
-        image = f"""
-        <div id="character-carousel" class="carousel slide rounded" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                {items}
-            </div>
-        </div>
-        """
-
-    return image_prop, image
+    return templates.TemplateResponse(
+        "profile.html",
+        {
+            "request": request,
+            "name": name,
+            "profile": profile,
+            "owner": user,
+            "guild": guild,
+            "spc": user == bot.bot.user,
+            "url": inconnu.profile_url(bio["_id"]),
+        },
+    )
 
 
 # Patch discord.Interaction

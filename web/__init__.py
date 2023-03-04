846f1f5e33a9f0902b16440e60c74455da42c74a
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-03-03 16:37:33 -0800

    Improve indentation

--- web/__init__.py
+++ web/__init__.py
@@ -2,4 +2,4 @@
 
 from fastapi.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="./web/templates")
+templates = Jinja2Templates(directory="./web/templates", trim_blocks=True, lstrip_blocks=True)

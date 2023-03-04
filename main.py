e1766a7c19704282c831ce3d1866316e7f1536eb
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-03-04 08:54:15 -0800

    Use Jinja2 templating (#18)
    Replace the old str.format()-based templating with Jinja2 (and make some tiny page tweaks).

--- main.py
+++ main.py
@@ -4,152 +4,20 @@ Invite: https://discord.com/api/oauth2/authorize?client_id=882409882119196704&pe
 """
 
 import asyncio
-import json
 import os
-from typing import Dict, List
 
 import discord
-import markdown
-from bson.objectid import ObjectId
 from fastapi import FastAPI
-from fastapi.exceptions import HTTPException
-from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from starlette import status
 
 import bot
-import inconnu
-from config import SHOW_TEST_ROUTES
 from logger import Logger
+from web.routers import base, profiles
 
 app = FastAPI(openapi_url=None)
-app.mount("/web", StaticFiles(directory="web"), name="web")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def home():
-    """Debug page or (if live) redirect to the documentation."""
-    if SHOW_TEST_ROUTES:
-        with open("web/index.html", "r", encoding="utf-8") as html:
-            return html.read()
-    return RedirectResponse("https://docs.inconnu.app", status_code=status.HTTP_303_SEE_OTHER)
-
-
-if SHOW_TEST_ROUTES:
-
-    @app.get("/test", response_class=HTMLResponse)
-    async def offline_page():
-        """Generate an offline test page."""
-        with open("web/snippets.json", "r", encoding="utf-8") as file:
-            profile = json.load(file)["sample"]
-            profile["user"] = 0
-            profile["guild"] = 0
-            return prepare_html(profile)
-
-
-@app.get("/profile/{charid}", response_class=HTMLResponse)
-async def display_character_profile(charid: str):
-    """Display character biography detail."""
-    if not ObjectId.is_valid(charid):
-        raise HTTPException(400, detail="Improper character ID.")
-
-    oid = ObjectId(charid)
-    bio = await inconnu.db.characters.find_one(
-        {"_id": oid},
-        {"name": 1, "user": 1, "guild": 1, "profile": 1},
-    )
-    if bio is None:
-        raise HTTPException(404, detail="Character not found.")
-
-    # Got the character; return the HTML
-    return prepare_html(bio)
-
-
-def prepare_html(bio: Dict[str, str | dict[str, str]]) -> str:
-    """Prep the character HTML page."""
-    with open("web/snippets.json", "r", encoding="utf-8") as snippets:
-        snippets = json.load(snippets)
-
-        # Basic profile info
-        name = bio["name"]
-        profile = bio.get("profile", {})
-        biography = markdown.markdown(profile.get("biography", "")) or snippets["unset"]
-        description = markdown.markdown(profile.get("description", "")) or snippets["unset"]
-
-        # Get the profile image by template
-        image_prop, image = generate_image_column(snippets, name, profile.get("images", []))
-
-        # Generate the ownership string and icons
-        guild = bot.bot.get_guild(bio["guild"])
-        user = guild.get_member(bio["user"]) if guild is not None else None
-        icons = get_icons(snippets, user, guild)
-
-        if guild is None:
-            ownership = snippets["unknown"]
-        elif user is None:
-            ownership = snippets["unowned"].format(guild=guild.name)
-        elif user == bot.bot.user:
-            ownership = snippets["spc"].format(guild=guild.name)
-        else:
-            # Guild found, user found, user not the bot
-            ownership = snippets["owned"].format(user=user.name, guild=guild.name)
-
-    with open("web/profile.html", "r", encoding="utf-8") as html_file:
-        html = html_file.read()
-        return html.format(
-            name=name,
-            url=inconnu.profile_url(bio["_id"]),
-            image_prop=image_prop,
-            biography=biography,
-            description=description,
-            image=image,
-            ownership=ownership,
-            icons="\n".join(icons),
-        )
-
-
-def get_icons(snippets, user, guild) -> List[str]:
-    """Get the icons for the user and guild."""
-    icons = []
-    if user is not None:
-        icons.append(
-            snippets["icon"].format(source=inconnu.get_avatar(user), name=user.display_name)
-        )
-    if guild is not None:
-        icons.append(snippets["icon"].format(source=guild.icon, name=guild.name))
-
-    return icons
-
-
-def generate_image_column(snippets, name, images):
-    """Generate the HTML for the image column."""
-    if not images:
-        image_prop = ""
-        image = snippets["no_profile_image"]
-    else:
-        image_prop = snippets["image_prop"].format(source=images[0])
-        carousel_items = []
-        for index, image in enumerate(images):
-            section_classes = "carousel-item"
-            if index == 0:
-                section_classes += " active"
-            carousel_items.append(
-                f"""
-                <div class="{section_classes}">
-                  <img src="{image}" class="d-block w-100" alt="{name}">
-                </div>
-                """
-            )
-        items = "\n".join(carousel_items)
-        image = f"""
-        <div id="character-carousel" class="carousel slide rounded" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                {items}
-            </div>
-        </div>
-        """
-
-    return image_prop, image
+app.mount("/public", StaticFiles(directory="./web/public"), name="public")
+app.include_router(base.router)
+app.include_router(profiles.router)
 
 
 # Patch discord.Interaction
---
+++ web/routers/base.py
@@ -0,0 +1,18 @@
+"""The base web route."""
+
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from starlette import status
+
+from config import SHOW_TEST_ROUTES
+from web import templates
+
+router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    """Debug page or (if live) redirect to the documentation site."""
+    if SHOW_TEST_ROUTES:
+        return templates.TemplateResponse("test.html.jinja", {"request": request})
+    return RedirectResponse("https://docs.inconnu.app", status_code=status.HTTP_303_SEE_OTHER)

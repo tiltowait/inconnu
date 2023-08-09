69c28d17ba9084c602bf241658a6f7aa773ead49
tiltowait <208040+tiltowait@users.noreply.github.com>
2023-08-09 15:45:27 -0700

    Add Easter egg

--- inconnu/misc/coinflip.py
+++ inconnu/misc/coinflip.py
@@ -5,5 +5,12 @@ import random
 
 async def coinflip(ctx):
     """Flip a coin."""
-    coin = random.choice(["Heads", "Tails"])
+    roll = random.randint(1, 1001)
+    if 1 <= roll <= 500:
+        coin = "Heads"
+    elif 501 <= roll <= 1000:
+        coin = "Tails"
+    else:
+        coin = "Landed on the edge"
+
     await ctx.respond(f"**{coin}!**")

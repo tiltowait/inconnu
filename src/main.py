ca2f5ef3702a911c21dc2c036af72f43f65415a6
tiltowait <208040+tiltowait@users.noreply.github.com>
2026-01-30 20:14:52 -0800

    Fix database race condition

--- src/main.py
+++ src/main.py
@@ -3,34 +3,30 @@ main.py - Start up the bot and perform any last-minute configuration.
 Invite: https://discord.com/api/oauth2/authorize?client_id=882409882119196704&permissions=2147829760&scope=bot%20applications.commands
 """
 
+import asyncio
 import os
 
-import discord
 import uvloop
 from dotenv import load_dotenv
 from loguru import logger
 
-import bot
+import db
+from bot import bot
 
 load_dotenv()
 
 
-# Patch discord.Interaction
-def respond(self, *args, **kwargs):
-    """Returns either the InteractionResponse or followup's send method."""
-    if self.response.is_done():
-        return self.followup.send(*args, **kwargs)
-    return self.response.send_message(*args, **kwargs)
+async def startup():
+    """Initialize the database connection and start the bot."""
+    await db.init()
+    async with bot:
+        await bot.start(os.environ["INCONNU_TOKEN"])
 
 
 def main():
     uvloop.install()
     logger.info("Installed uvloop")
-
-    discord.Interaction.respond = respond
-    logger.info("Patched discord.Interaction")
-
-    bot.bot.run(os.environ["INCONNU_TOKEN"])
+    asyncio.run(startup())
 
 
 if __name__ == "__main__":

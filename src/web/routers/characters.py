5ce42e9a85fd1fa921f2f23a385d5628fa9c22dd
tiltowait <208040+tiltowait@users.noreply.github.com>
2026-01-31 23:43:29 -0800

    Add /characters endpoint

--- src/web/routers/characters.py
+++ src/web/routers/characters.py
@@ -1,7 +1,8 @@
 """Character API router."""
 
-from typing import Optional
+from typing import Optional, Self
 
+import discord
 from beanie import PydanticObjectId
 from fastapi import APIRouter, Depends, Request
 from fastapi.exceptions import HTTPException
@@ -13,6 +14,8 @@ from config import API_KEY
 from models import VChar
 from services import char_mgr
 
+DISCORD_HEADER = "X-Discord-User-ID"
+
 router = APIRouter()
 
 
@@ -23,12 +26,45 @@ class Guild(BaseModel):
     name: str
     icon: Optional[str]
 
+    @classmethod
+    async def fetch(cls, id: int) -> Self:
+        """Fetch guild data from Discord."""
+        try:
+            # This method raises, despite the docs' indication otherwise, so we
+            # have to wrap it in a try, irkshome though that is.
+            guild = await inconnu.bot.get_or_fetch_guild(id)
+            if guild is None:
+                # This can probably never happen, but the return type hints
+                # are wrong
+                return cls.unknown(id)
+            return cls.create(guild)
+        except Exception:
+            return cls.unknown(id)
+
+    @classmethod
+    def create(cls, guild: discord.Guild) -> Self:
+        """Create from a real Discord guild object."""
+        icon = guild.icon.url if guild.icon else None
+        return cls(id=guild.id, name=guild.name, icon=icon)
+
+    @classmethod
+    def unknown(cls, id: int) -> Self:
+        """Return generic data."""
+        return cls(id=id, name="Unknown", icon=None)
+
 
 class CharacterReturn(BaseModel):
     """The character data for /characters/{oid}."""
 
     guild: Guild
     character: VChar
+
+
+class CharacterList(BaseModel):
+    """The character data for /characters."""
+
+    guilds: list[Guild]
+    characters: list[VChar]
 
 
 async def verify_api_key(
@@ -40,6 +76,33 @@ async def verify_api_key(
     return credentials
 
 
+def get_discord_user_id(request: Request) -> int:
+    """Get the Discord user ID from the request headers."""
+    user_id = request.headers.get(DISCORD_HEADER)
+    if user_id is None:
+        raise HTTPException(400, detail="User ID missing")
+    return int(user_id)
+
+
+@router.get("/characters")
+async def get_character_list(
+    request: Request,
+    _: HTTPAuthorizationCredentials = Depends(verify_api_key),
+) -> CharacterList:
+    """Get all of the user's characters."""
+    user_id = get_discord_user_id(request)
+
+    guilds: dict[int, Guild] = {}
+    chars = []
+    async for char in VChar.find(VChar.user == user_id):
+        if char.guild not in guilds:
+            guild = await Guild.fetch(char.guild)
+            guilds[char.guild] = guild
+        chars.append(char)
+
+    return CharacterList(guilds=list(guilds.values()), characters=chars)
+
+
 @router.get("/characters/{oid}")
 async def get_character(
     request: Request,
@@ -47,24 +110,17 @@ async def get_character(
     _: HTTPAuthorizationCredentials = Depends(verify_api_key),
 ) -> CharacterReturn:
     """Returns a given character if it belongs to the authed user."""
-    user_id = request.headers.get("X-Discord-User-ID")
-    if user_id is None:
-        raise HTTPException(400, detail="Missing user ID")
-
+    user_id = get_discord_user_id(request)
     char = await char_mgr.id_fetch(oid)
+
     if char is None:
         raise HTTPException(404, detail="Character not found")
     if int(user_id) != char.user:
         raise HTTPException(403, detail="User does not own character")
 
-    guild = await inconnu.bot.get_or_fetch_guild(char.guild)
-    if guild is not None:
-        icon = guild.icon.url if guild.icon else None
-        guild_data = Guild(id=guild.id, name=guild.name, icon=icon)
-    else:
-        guild_data = Guild(id=char.guild, name="Unknown", icon=None)
+    guild = await Guild.fetch(char.guild)
 
     return CharacterReturn(
-        guild=guild_data,
+        guild=guild,
         character=char,
     )

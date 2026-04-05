ee4abffd5491a5911b76a687ea49708e7197b9e7
tiltowait <208040+tiltowait@users.noreply.github.com>
2026-04-05 09:51:52 -0700

    Refactor header edit

---
+++ src/inconnu/header/fix/__init__.py
@@ -0,0 +1,5 @@
+"""Header fix operations for posted headers."""
+
+from inconnu.header.fix.location import fix_header_location
+
+__all__ = ("fix_header_location",)
--- src/interface/header.py
+++ src/interface/header.py
@@ -21,108 +21,6 @@ if TYPE_CHECKING:
     from bot import InconnuBot
 
 
-class LocationChangeModal(discord.ui.Modal):
-    """A modal for changing RP header location."""
-
-    def __init__(self, header: discord.Message, webhook: discord.Webhook | None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.header = header
-        self.webhook = webhook
-        self.updating_title = webhook is None
-
-        self.add_item(
-            discord.ui.InputText(
-                label="New Location",
-                placeholder="The location where the scene takes place",
-                value=self.get_location(),
-                max_length=100,
-            )
-        )
-        self.add_item(
-            discord.ui.InputText(
-                label="Temporary Effects",
-                placeholder="Temporary effects relevant to the scene",
-                value=getattr(header.embeds[0].footer, "text", ""),
-                max_length=512,
-                required=False,
-            )
-        )
-
-    def get_location(self):
-        """Get the header's current location."""
-        if self.updating_title:
-            elements = self.header.embeds[0].title.split(" • ")
-        else:
-            elements = self.header.embeds[0].author.name.split(" • ")
-
-        if len(elements) == 1:
-            # Character name isn't given when the header is in the author
-            # field. If we're looking at the embed title, then, no location
-            # exists; otherwise, it's the first and only element.
-            return "" if self.updating_title else elements[0]
-        if len(elements) == 2:
-            if self.updating_title:
-                if "Blushed" in elements[-1]:
-                    return ""
-                return elements[-1]
-
-            return elements[0]
-
-        # 3 elements; this could also be elements[1]
-        return elements[-2]
-
-    async def callback(self, interaction: discord.Interaction):
-        """Update the RP header."""
-        location = " ".join(self.children[0].value.split())
-        temp_effects = " ".join(self.children[1].value.split())
-        embed = self.header.embeds[0]
-
-        # Some headers have a title; others use the author string
-        if self.updating_title:
-            # The title contains name, blush, location, but only name is guaranteed
-            logger.debug("EDIT HEADER: Embed has a title")
-            elements = embed.title.split(" • ")
-        else:
-            logger.debug("EDIT HEADER: Embed does not have a title")
-            elements = embed.author.name.split(" • ")
-
-        if self.get_location():
-            # We need to remove the old location
-            if self.updating_title:
-                del elements[1]
-            else:
-                del elements[0]
-
-        # Generate the new heading
-        insertion_index = 1 if self.updating_title else 0
-        elements.insert(insertion_index, location)
-        new_heading = " • ".join(elements)
-
-        if self.updating_title:
-            embed.title = new_heading
-        else:
-            url = embed.author.url
-            icon_url = embed.author.icon_url
-            embed.set_author(name=new_heading, url=url, icon_url=icon_url)
-
-        embed.set_footer(text=temp_effects)
-
-        if self.webhook is None:
-            logger.debug("EDIT HEADER: Updating with Message.edit()")
-            await self.header.edit(embed=embed)
-        else:
-            logger.debug("EDIT HEADER: Updating with Webhook.edit_message()")
-            await self.webhook.edit_message(self.header.id, embed=embed)
-
-        # Inform the user
-        temp_effects = temp_effects or "*None*"
-        embed = discord.Embed(
-            title="Header Updated",
-            description=f"**Location:** {location}\n**Temporary Effects:** {temp_effects}",
-        )
-        await interaction.response.send_message(embed=embed, ephemeral=True, delete_after=5)
-
-
 class HeaderCog(commands.Cog):
     """A cog with header-related commands, including context menu commands."""
 
@@ -189,44 +87,9 @@ class HeaderCog(commands.Cog):
         await inconnu.header.update_header(ctx, character)
 
     @commands.message_command(name="Header: Edit", contexts={discord.InteractionContextType.guild})
-    async def fix_rp_header(self, ctx, message: discord.Message):
+    async def fix_rp_header(self, ctx: AppCtx, message: discord.Message):
         """Change an RP header's location."""
-        possible_header = False
-        webhook: discord.Webhook | None = None
-
-        if message.author == self.bot.user:
-            possible_header = True
-        else:
-            try:
-                webhook = await self.bot.webhook_cache.fetch_webhook(ctx.channel, message.author.id)
-                if webhook is not None:
-                    logger.info("EDIT HEADER: Editing a WebhookMessage")
-                    possible_header = True
-                else:
-                    logger.debug("EDIT HEADER: Not a WebhookMessage")
-            except discord.errors.Forbidden:
-                logger.info("EDIT HEADER: No webhook permissions")
-
-        if possible_header:
-            # Make sure we have a header
-            record = await db.headers.find_one({"message": message.id})
-            if record is not None:
-                # Make sure we are allowed to update it
-                owner = record["character"]["user"]
-                if ctx.user.id == owner:
-                    # Modal gets the new location
-                    logger.debug("HEADER: {} is updating an RP header", ctx.user.name)
-                    modal = LocationChangeModal(message, webhook, title="Edit RP Header")
-                    await ctx.send_modal(modal)
-                else:
-                    logger.debug(
-                        "HEADER: Unauthorized RP header update attempt by {}", ctx.user.name
-                    )
-                    await ctx.respond("This isn't your RP header!", ephemeral=True)
-                return
-
-        logger.debug("HEADER: {} attempted to update a non-header post", ctx.user.name)
-        await ctx.respond("This message isn't an RP header!", ephemeral=True)
+        await inconnu.header.fix.fix_header_location(ctx, message)
 
     @commands.message_command(
         name="Header: Delete",
